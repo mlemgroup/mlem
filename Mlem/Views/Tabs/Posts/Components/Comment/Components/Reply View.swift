5130d0d542c25fbce7df5ddfd5282530fa74ea6a
David Bureš <buresdv@gmail.com>
2022-03-31 20:49:47 +0200

    Lepší reply view

--- Mlem/Views/Tabs/Posts/Components/Comment/Components/Reply View.swift
+++ Mlem/Views/Tabs/Posts/Components/Comment/Components/Reply View.swift
@@ -35,17 +35,24 @@ struct Reply_View: View {
                 }
             }
             
-            Text("\(parentCommentAuthor):")
-                .dynamicTypeSize(.small)
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-            Text(.init(parentCommentText))
-                .dynamicTypeSize(.small)
-                .font(.subheadline)
+            VStack(alignment: .leading, spacing: 3) {
+                Text(parentCommentAuthor)
+                    .foregroundColor(.secondary)
+                Text(.init(parentCommentText))
+                    .padding()
+                    .background(Color.secondarySystemBackground)
+                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .circular))
+            }
+            .dynamicTypeSize(.small)
+            .font(.subheadline)
+            .multilineTextAlignment(.leading)
             
             TextEditor(text: $replyContent)
+                .lineSpacing(4)
                 .clipShape(RoundedRectangle(cornerRadius: 2))
                 .border(Color.secondarySystemBackground, width: 2)
+            
+            Spacer()
         }
         .padding()
     }
