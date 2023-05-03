2d5ea35a023bd223b5bc9ab083842b1f4bbad098
David Bureš <buresdv@gmail.com>
2023-05-03 16:08:48 +0200

    + Basic Utilities

---
+++ Mlem/App Constants.swift
@@ -0,0 +1,13 @@
+//
+//  App Constants.swift
+//  Mlem
+//
+//  Created by David Bureš on 03.05.2023.
+//
+
+import Foundation
+
+struct AppConstants
+{
+    static let webSocketSession: URLSession = URLSession(configuration: .default)
+}
---
+++ Mlem/Logic/Networking/Send Command.swift
@@ -0,0 +1,14 @@
+//
+//  Send Command.swift
+//  Mlem
+//
+//  Created by David Bureš on 03.05.2023.
+//
+
+import Foundation
+import SwiftyJSON
+
+func sendCommand(maintainOpenConnection: Bool, command: String) async throws -> JSON
+{
+    
+}
