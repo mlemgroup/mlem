244a6616cb766b588f3b45fd85fd27d66bde321e
David Bureš <buresdv@gmail.com>
2023-05-16 19:06:30 +0200

    + Sections for favorited communities and search results

--- Mlem/Views/Tabs/Posts/Components/Post/Components/Community View/Community Search/Community Search View.swift
+++ Mlem/Views/Tabs/Posts/Components/Post/Components/Community View/Community Search/Community Search View.swift
@@ -28,19 +28,26 @@ struct CommunitySearchResultsView: View
             {
                 if !getFavoritedCommunitiesForAccount(account: account, tracker: favoritedCommunitiesTracker).isEmpty
                 {
-                    List(getFavoritedCommunitiesForAccount(account: account, tracker: favoritedCommunitiesTracker))
-                    { favoritedCommunity in
-                        NavigationLink(destination: CommunityView(instanceAddress: instanceAddress, account: account, community: favoritedCommunity.community))
-                        {
-                            Text(favoritedCommunity.community.name)
-                                .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                                    Button(role: .destructive) {
-                                        unfavoriteCommunity(account: account, community: favoritedCommunity.community, favoritedCommunitiesTracker: favoritedCommunitiesTracker)
-                                    } label: {
-                                        Label("Unfavorite", systemImage: "star.slash")
-                                    }
-
+                    List
+                    {
+                        Section {
+                            ForEach(getFavoritedCommunitiesForAccount(account: account, tracker: favoritedCommunitiesTracker))
+                            { favoritedCommunity in
+                                NavigationLink(destination: CommunityView(instanceAddress: instanceAddress, account: account, community: favoritedCommunity.community))
+                                {
+                                    Text(favoritedCommunity.community.name)
+                                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                            Button(role: .destructive) {
+                                                unfavoriteCommunity(account: account, community: favoritedCommunity.community, favoritedCommunitiesTracker: favoritedCommunitiesTracker)
+                                            } label: {
+                                                Label("Unfavorite", systemImage: "star.slash")
+                                            }
+                                            
+                                        }
                                 }
+                            }
+                        } header: {
+                            Text("Favorites")
                         }
                     }
                 }
@@ -54,30 +61,37 @@ struct CommunitySearchResultsView: View
             }
             else
             {
-                List(communitySearchResultsTracker.foundCommunities)
-                { foundCommunity in
-                    NavigationLink(destination: CommunityView(instanceAddress: instanceAddress, account: account, community: foundCommunity))
-                    {
-                        Text(foundCommunity.name)
-                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                                if favoritedCommunitiesTracker.favoriteCommunities.contains(where: { $0.community.id == foundCommunity.id })
-                                { /// This is when a community is already favorited
-                                    Button(role: .destructive) {
-                                        unfavoriteCommunity(account: account, community: foundCommunity, favoritedCommunitiesTracker: favoritedCommunitiesTracker)
-                                    } label: {
-                                        Label("Unfavorite", systemImage: "star.slash")
+                List
+                {
+                    Section {
+                        ForEach(communitySearchResultsTracker.foundCommunities)
+                        { foundCommunity in
+                            NavigationLink(destination: CommunityView(instanceAddress: instanceAddress, account: account, community: foundCommunity))
+                            {
+                                Text(foundCommunity.name)
+                                    .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                        if favoritedCommunitiesTracker.favoriteCommunities.contains(where: { $0.community.id == foundCommunity.id })
+                                        { /// This is when a community is already favorited
+                                            Button(role: .destructive) {
+                                                unfavoriteCommunity(account: account, community: foundCommunity, favoritedCommunitiesTracker: favoritedCommunitiesTracker)
+                                            } label: {
+                                                Label("Unfavorite", systemImage: "star.slash")
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Button {
+                                                favoriteCommunity(account: account, community: foundCommunity, favoritedCommunitiesTracker: favoritedCommunitiesTracker)
+                                            } label: {
+                                                Label("Favorite", systemImage: "star")
+                                            }
+                                            .tint(.yellow)
+                                        }
                                     }
-                                }
-                                else
-                                {
-                                    Button {
-                                        favoriteCommunity(account: account, community: foundCommunity, favoritedCommunitiesTracker: favoritedCommunitiesTracker)
-                                    } label: {
-                                        Label("Favorite", systemImage: "star")
-                                    }
-                                    .tint(.yellow)
-                                }
                             }
+                        }
+                    } header: {
+                        Text("Search Results")
                     }
                 }
             }
