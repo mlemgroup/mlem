ac65ee177ec74faf850eb8fe0d6829ad6cbcb414
David Bureš <buresdv@gmail.com>
2023-06-05 00:27:22 +0200

    ^ Obtaining tokens ^ Feed loader

--- Mlem/Logic/Networking/Infinite Loader.swift
+++ Mlem/Logic/Networking/Infinite Loader.swift
@@ -15,36 +15,37 @@ internal enum LoadingError
 
 func loadInfiniteFeed(postTracker: PostTracker, appState: AppState, community: Community?, feedType: FeedType, sortingType: SortingOptions, account: SavedAccount) async
 {
-    var loadingCommand: String = ""
+    var loadingParameters: [URLQueryItem] = []
     
     if let community
     {
         print("Will be in COMMUNITY scope")
         
-        loadingCommand = """
-        {"op": "GetPosts", "data": {"auth": "\(account.accessToken)", "type_": "All", "sort": "\(sortingType.rawValue)", "page": \(postTracker.page), "community_id": \(community.id)}}
-        """
+        loadingParameters = [
+            URLQueryItem(name: "type_", value: feedType.rawValue),
+            URLQueryItem(name: "sort", value: sortingType.rawValue),
+            URLQueryItem(name: "page", value: "\(postTracker.page)"),
+            URLQueryItem(name: "community_id", value: "\(community.id)")
+        ]
     }
     else
     {
         print("Will be in GLOBAL scope")
         
-        loadingCommand = """
-        {"op": "GetPosts", "data": {"auth": "\(account.accessToken)", "type_": "\(feedType.rawValue)", "sort": "\(sortingType.rawValue)", "page": \(postTracker.page)}}
-        """
+        loadingParameters = [
+            URLQueryItem(name: "type_", value: feedType.rawValue),
+            URLQueryItem(name: "sort", value: sortingType.rawValue),
+            URLQueryItem(name: "page", value: "\(postTracker.page)"),
+        ]
     }
 
     print("Page counter value: \(postTracker.page)")
     
-    print("Will try to send command: \(loadingCommand)")
+    print("Will try to send these parameters: \(loadingParameters)")
     
     do
     {
-        let apiResponse = try await sendGetCommand(account: account, endpoint: "/post/list", parameters: [
-            URLQueryItem(name: "type_", value: feedType.rawValue),
-            URLQueryItem(name: "sort", value: sortingType.rawValue),
-            URLQueryItem(name: "page", value: "1")
-        ])
+        let apiResponse = try await sendGetCommand(account: account, endpoint: "post/list", parameters: loadingParameters)
         
         //let apiResponse = try await sendCommand(maintainOpenConnection: true, instanceAddress: account.instanceLink, command: loadingCommand)
         
