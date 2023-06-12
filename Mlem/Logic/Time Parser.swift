0593b986d09aa4e840e1c7ed4823c300ee5a1495
Nick Lawson <nick@mormaer.dev>
2023-06-12 10:25:06 +0100

    treat relative values with `in` prefix as `now`

--- Mlem/Logic/Time Parser.swift
+++ Mlem/Logic/Time Parser.swift
@@ -14,5 +14,7 @@ func getTimeIntervalFromNow(date: Date) -> String
     AppConstants.relativeDateFormatter.formattingContext = .standalone
     AppConstants.relativeDateFormatter.calendar = .autoupdatingCurrent
     
-    return String(AppConstants.relativeDateFormatter.localizedString(for: date, relativeTo: .now).dropLast(4)) /// Drop the last 4 characters, because all of these strings have "ago" (for example "3 hr ago"), and we don't want that "ago" to be there
+    // Drop the last 4 characters, because all of these strings have "ago" (for example "3 hr ago"), and we don't want that "ago" to be there
+    let value = String(AppConstants.relativeDateFormatter.localizedString(for: date, relativeTo: .now).dropLast(4))
+    return value.hasPrefix("in") ? "now" : value
 }
