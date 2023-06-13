62aa24aecc16dc5c3e48e749ea56575488d32a38
Nick Lawson <nick@mormaer.dev>
2023-06-13 22:37:57 +0100

    fix #68 - early return for unsupported url schemes

--- Mlem/Logic/URLHandler.swift
+++ Mlem/Logic/URLHandler.swift
@@ -15,7 +15,12 @@ class URLHandler {
     init() { /* class should not be instantiated */ }
     
     static func handle(_ url: URL) -> OpenURLAction.Result {
-        #warning("TODO: consider how we might deep link within the application for urls such as '/c/<community_name>' and '/post/<post_id>'")
+        guard let scheme = url.scheme, scheme.hasPrefix("http") else {
+            // as the current handling only supports http(s) via Safari bail early if the scheme is unsupported...
+            // a future piece of work will add deep linking where posssible with help of the ResolveObject API call
+            return .discarded
+        }
+        
         let viewController = SFSafariViewController(url: url, configuration: .default)
         UIApplication.shared.firstKeyWindow?.rootViewController?.present(viewController, animated: true)
         return .handled
