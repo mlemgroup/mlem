cbf7bfa691319507443be032bf33a566d2950349
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-06-15 11:51:41 -0400

    BROKEN WORKING ON SETTING

--- Mlem/Views/Tabs/Settings/Components/Settings Item.swift
+++ Mlem/Views/Tabs/Settings/Components/Settings Item.swift
@@ -1,3 +1,33 @@
+////
+////  Settings Item.swift
+////  Mlem
+////
+////  Created by David Bureš on 26.03.2022.
+////
+//
+//import SwiftUI
+//
+//struct SettingsItem: View
+//{
+//    @State var settingPictureSystemName: String
+//    @State var settingPictureColor: Color
+//
+//    @State var settingName: String
+//
+//    @Binding var isTicked: Bool
+//
+//    var body: some View
+//    {
+//        HStack
+//        {
+//            Image(systemName: settingPictureSystemName)
+//                .foregroundColor(settingPictureColor)
+//
+//            Toggle(settingName, isOn: $isTicked)
+//        }
+//    }
+//}
+
 //
 //  Settings Item.swift
 //  Mlem
@@ -7,7 +37,7 @@
 
 import SwiftUI
 
-struct SettingsItem: View
+struct SwitchableSettingsItem: View
 {
     @State var settingPictureSystemName: String
     @State var settingPictureColor: Color
@@ -24,6 +54,43 @@ struct SettingsItem: View
                 .foregroundColor(settingPictureColor)
 
             Toggle(settingName, isOn: $isTicked)
+        }
+    }
+}
+
+struct SelectableSettingsItem<T: SettingsOptions>: View
+{
+    @State var settingPictureSystemName: String
+    @State var settingPictureColor: Color
+
+    @State var settingName: String
+
+    @Binding var currentValue: T?
+    @State var options: [T]
+
+    var body: some View
+    {
+        NavigationLink {
+            List(options, selection: $currentValue) { option in
+                HStack {
+                    Text(option.label)
+                    if option == currentValue {
+                        Spacer()
+                        Image(systemName: "checkmark")
+                    }
+                }
+            }
+        } label: {
+            HStack
+            {
+                Image(systemName: settingPictureSystemName)
+                    .foregroundColor(settingPictureColor)
+
+                Text(settingName)
+                Spacer()
+                Text(currentValue?.label ?? "-")
+                    .foregroundColor(.secondary)
+            }
         }
     }
 }
