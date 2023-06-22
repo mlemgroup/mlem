a04c54e2050d1d7dbd0db83c9e377cd6f0ad7c78
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-06-21 22:30:01 -0400

    tidying

--- Mlem/API/APIClient.swift
+++ Mlem/API/APIClient.swift
@@ -30,7 +30,6 @@ class APIClient {
     }
 
     func perform<Request: APIRequest>(request: Request) async throws -> Request.Response {
-        // print(request)
         
         let urlRequest = try urlRequest(from: request)
         let (data, response) = try await execute(urlRequest)
