2caf86a83a6de95527f0ac5256896e29ec548252
Itay sharzer <itay@myinterview.com>
2023-06-23 19:02:35 +0300

    - updated

--- Mlem/Views/Tabs/Posts/Components/Post/Components/Community View/Post Item/Feed Post.swift
+++ Mlem/Views/Tabs/Posts/Components/Post/Components/Community View/Post Item/Feed Post.swift
@@ -97,9 +97,6 @@ struct FeedPost: View {
                 Divider()
             }
         }
-        .contextMenu {
-            EasyTranslateButton(text: .constant(postView.post.body))
-        }
         .sheet(isPresented: $isComposingReport) {
             ReportComposerView(account: account, reportedPost: postView)
         }
