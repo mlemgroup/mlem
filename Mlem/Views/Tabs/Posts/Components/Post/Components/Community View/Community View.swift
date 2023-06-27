5c0c4e3046c8ce245b26e74212bf1e061e65d58a
Itay sharzer <itay@myinterview.com>
2023-06-27 22:37:32 +0300

    - merged the linter

--- Mlem/Views/Tabs/Posts/Components/Post/Components/Community View/Community View.swift
+++ Mlem/Views/Tabs/Posts/Components/Post/Components/Community View/Community View.swift
@@ -366,6 +366,9 @@ struct CommunityView: View {
                     replyToPost: replyToPost
                 )
             }
+            .contextMenu {
+                EasyTranslateButton(text: .constant(post.post.body))
+            }
             .buttonStyle(EmptyButtonStyle()) // Make it so that the link doesn't mess with the styling
             .onAppear {
                 Task(priority: .medium) {
