30705d2433ca5987fbd8fe4ba00b4bec7d2b3c98
mormaer <mormaer@users.noreply.github.com>
2023-07-02 16:11:57 +0100

    fix - avoid mapping unused values in `APILocalSite` (#417)

--- Mlem/API/Models/Site/APILocalSite.swift
+++ Mlem/API/Models/Site/APILocalSite.swift
@@ -9,36 +9,36 @@ import Foundation
 
 // lemmy_db_schema::source::local_site::LocalSite
 struct APILocalSite: Decodable {
-    let id: Int
-    let siteId: Int
-    let siteSetup: Bool
+//    let id: Int
+//    let siteId: Int
+//    let siteSetup: Bool
     let enableDownvotes: Bool
-    let enableNsfw: Bool
-    let communityCreationAdminOnly: Bool
-    let requireEmailVerification: Bool
-    let applicationQuestion: String?
-    let privateInstance: Bool
-    let defaultTheme: String
-    let defaultPostListingType: String
-    let legalInformation: String?
-    let hideModlogModNames: Bool
-    let applicationEmailAdmins: Bool
-    let slurFilterRegex: String?
-    let actorNameMaxLength: Int
-    let federationEnabled: Bool
-    let federationDebug: Bool
-    let federationWorkerCount: Int
-    let captchaEnabled: Bool
-    let captchaDifficulty: String
-    let registrationMode: APIRegistrationMode
-    let reportsEmailAdmins: Bool
-    let published: Date
-    let updated: Date?
+//    let enableNsfw: Bool
+//    let communityCreationAdminOnly: Bool
+//    let requireEmailVerification: Bool
+//    let applicationQuestion: String?
+//    let privateInstance: Bool
+//    let defaultTheme: String
+//    let defaultPostListingType: String
+//    let legalInformation: String?
+//    let hideModlogModNames: Bool
+//    let applicationEmailAdmins: Bool
+//    let slurFilterRegex: String?
+//    let actorNameMaxLength: Int
+//    let federationEnabled: Bool
+//    let federationDebug: Bool
+//    let federationWorkerCount: Int
+//    let captchaEnabled: Bool
+//    let captchaDifficulty: String
+//    let registrationMode: APIRegistrationMode
+//    let reportsEmailAdmins: Bool
+//    let published: Date
+//    let updated: Date?
 }
 
 // lemmy_db_schema::source::local_site::RegistrationMode
 enum APIRegistrationMode: String, Codable {
-    case closed = "closed"
-    case requireApplication = "requireapplication"
-    case open = "open"
+    case closed = "Closed"
+    case requireApplication = "RequireApplication"
+    case open = "Open"
 }
