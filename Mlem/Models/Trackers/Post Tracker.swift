26a7ece4b3145c26a1134449576a9f63499f4875
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-07-07 12:58:52 -0400

    updated website icon complex, added preloading for avatars

--- Mlem/Models/Trackers/Post Tracker.swift
+++ Mlem/Models/Trackers/Post Tracker.swift
@@ -95,10 +95,21 @@ class PostTracker: FeedTracker<APIPostView> {
                 if let url = url {
                     imageRequests.append(ImageRequest(url: url, priority: .high))
                 }
+            case .link(let url):
+                // websites: load image and favicon
+                Task(priority: .userInitiated) {
+                    if let baseURL = postView.post.url?.host,
+                       let favIconURL = URL(string: "https://www.google.com/s2/favicons?sz=64&domain=\(baseURL)") {
+                        await preloadSingleImage(url: favIconURL)
+                    }
+                    if let url = url {
+                        await preloadSingleImage(url: url)
+                    }
+                }
             default:
                 break
             }
-
+            
         }
 
         prefetcher.startPrefetching(with: imageRequests)
