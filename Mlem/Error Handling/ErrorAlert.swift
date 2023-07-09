4a74b78e17cdee84b47fa1fe5db977c0ab0ac736
mormaer <nick@mormaer.dev>
2023-07-09 12:50:15 +0100

    add `EquatableError` and `ContextualError`

---
+++ Mlem/Error Handling/ErrorAlert.swift
@@ -0,0 +1,23 @@
+// 
+//  ErrorAlert.swift
+//  Mlem
+//
+//  Created by mormaer on 09/07/2023.
+//  
+//
+
+import Foundation
+
+struct ErrorAlert {
+    let title: String
+    let message: String
+}
+
+extension ErrorAlert {
+    static var unexpected: Self {
+        .init(
+            title: "Something went wrong",
+            message: "Sorry, something unexpected happened. Please try again"
+        )
+    }
+}
