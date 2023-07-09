4ca1ccb69028fc0f003e6ca75a7db07a76e4777d
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-07-09 09:32:49 -0400

    tmp

---
+++ Mlem/Views/Tabs/Settings/Components/Views/Privacy Policy.swift
@@ -0,0 +1,15 @@
+//
+//  Privacy Policy.swift
+//  Mlem
+//
+//  Created by Eric Andrews on 2023-07-09.
+//
+
+import Foundation
+import SwiftUI
+
+struct privacyPolicy: View {
+    var body: some View {
+        MarkdownView(text: <#T##String#>, isNsfw: <#T##Bool#>)
+    }
+}
