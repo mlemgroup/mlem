959dbd690078ea149e4e59afd4349429f7095799
Weston Hanners <864259+WestonHanners@users.noreply.github.com>
2023-07-11 13:19:21 -0400

    Fixed Catch-all

--- Mlem/Logic/Participating/BlockPersonLogic.swift
+++ Mlem/Logic/Participating/BlockPersonLogic.swift
@@ -12,15 +12,11 @@ func blockPerson(
     account: SavedAccount,
     person: APIPerson,
     blocked: Bool) async throws -> Bool {
-        do {
-            let request = BlockPersonRequest(
-                account: account,
-                personId: person.id,
-                block: blocked
-            )
-            let response = try await APIClient().perform(request: request)
-            return response.blocked
-        } catch {
-            return false
-        }
+        let request = BlockPersonRequest(
+            account: account,
+            personId: person.id,
+            block: blocked
+        )
+        let response = try await APIClient().perform(request: request)
+        return response.blocked
     }
