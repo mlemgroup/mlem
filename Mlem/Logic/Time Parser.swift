ba9249c84b7bd771a489c820daa06e74e0e92f83
Weston Hanners <864259+WestonHanners@users.noreply.github.com>
2023-07-13 23:21:27 -0400

    I dont think this did anything.

--- Mlem/Logic/Time Parser.swift
+++ Mlem/Logic/Time Parser.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
-func getTimeIntervalFromNow(date: Date, unitsStyle: RelativeDateTimeFormatter.UnitsStyle = .abbreviated) -> String {
-    AppConstants.relativeDateFormatter.unitsStyle = unitsStyle
-    return AppConstants.relativeDateFormatter.localizedString(for: date, relativeTo: Date())
+func getTimeIntervalFromNow(date: Date, unitsStyle: DateComponentsFormatter.UnitsStyle = .abbreviated) -> String {
+    AppConstants.dateComponentsFormatter.unitsStyle = unitsStyle
+    AppConstants.dateComponentsFormatter.maximumUnitCount = 1
+    
+    let interval = date.timeIntervalSinceNow
+    if interval > -1 {
+        return "Now"
+    }
+    
+    let value = AppConstants.dateComponentsFormatter.string(from: abs(interval))
+    return value ?? "Unknown"
 }
