4b3e01c77e9cd0137efe20158ea1194a456fd69d
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-07-18 01:20:53 -0400

    i hate it here

--- Mlem/ContentView.swift
+++ Mlem/ContentView.swift
@@ -15,41 +15,50 @@ struct ContentView: View {
     @State private var errorAlert: ErrorAlert?
     @State private var expiredSessionAccount: SavedAccount?
 
-    @State private var tabSelection = 1
+    @State private var tabSelection: Int = 1
 
     @AppStorage("showUsernameInNavigationBar") var showUsernameInNavigationBar: Bool = true
 
     var body: some View {
-        TabView(selection: $tabSelection) {
+        // TabView(selection: $tabSelection) {
+        FancyTabBar(selection: $tabSelection) {
             FeedRoot()
-                .tabItem {
+                .fancyTabItem(tag: 1) {
                     Label("Feeds", systemImage: "scroll")
                         .environment(\.symbolVariants, tabSelection == 1 ? .fill : .none)
-                }.tag(1)
+                }
+//                .tabItem {
+//                    Label("Feeds", systemImage: "scroll")
+//                        .environment(\.symbolVariants, tabSelection == 1 ? .fill : .none)
+//                }.tag(1)
 
             InboxView()
-                .tabItem {
+                .fancyTabItem(tag: 2) {
                     Label("Inbox", systemImage: "mail.stack")
                         .environment(\.symbolVariants, tabSelection == 2 ? .fill : .none)
-                }.tag(2)
-
-            ProfileView(userID: appState.currentActiveAccount.id)
-                .tabItem {
-                    Label(computeUsername(account: appState.currentActiveAccount), systemImage: "person.circle")
-                        .environment(\.symbolVariants, tabSelection == 3 ? .fill : .none)
                 }
-                .tag(3)
+//                .tabItem {
+//                    Label("Inbox", systemImage: "mail.stack")
+//                        .environment(\.symbolVariants, tabSelection == 2 ? .fill : .none)
+//                }.tag(2)
 
-            SearchView()
-                .tabItem {
-                    Label("Search", systemImage: tabSelection == 4 ? "text.magnifyingglass" : "magnifyingglass")
-                }.tag(4)
-
-            SettingsView()
-                .tabItem {
-                    Label("Settings", systemImage: "gear")
-                        .environment(\.symbolVariants, tabSelection == 5 ? .fill : .none)
-                }.tag(5)
+//            ProfileView(userID: appState.currentActiveAccount.id)
+//                .tabItem {
+//                    Label(computeUsername(account: appState.currentActiveAccount), systemImage: "person.circle")
+//                        .environment(\.symbolVariants, tabSelection == 3 ? .fill : .none)
+//                }
+//                .tag(3)
+//
+//            SearchView()
+//                .tabItem {
+//                    Label("Search", systemImage: tabSelection == 4 ? "text.magnifyingglass" : "magnifyingglass")
+//                }.tag(4)
+//
+//            SettingsView()
+//                .tabItem {
+//                    Label("Settings", systemImage: "gear")
+//                        .environment(\.symbolVariants, tabSelection == 5 ? .fill : .none)
+//                }.tag(5)
         }
         .onChange(of: appState.contextualError) { handle($0) }
         .alert(using: $errorAlert) { content in
---
+++ Mlem/Custom Tab Bar/FancyTabBar.swift
@@ -0,0 +1,59 @@
+//
+//  FancyTabBar.swift
+//  Mlem
+//
+//  Created by Eric Andrews on 2023-07-17.
+//
+
+import Foundation
+import SwiftUI
+
+struct FancyTabBar<Selection: Hashable, Content: View>: View {
+    
+    @Binding private var selection: Selection
+    private let content: () -> Content
+    
+    @State private var tabItemKeys: [Selection] = []
+    @State private var tabItems: [Selection: FancyTabItemLabelBuilder<Selection>] = [:]
+    
+    init(selection: Binding<Selection>,
+         @ViewBuilder content: @escaping () -> Content) {
+        self._selection = selection
+        self.content = content
+    }
+    
+    var body: some View {
+        content()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .safeAreaInset(edge: .bottom) {
+                VStack(spacing: 0) {
+                    Divider()
+                    
+                    HStack {
+                        ForEach(tabItemKeys, id: \.hashValue) { key in
+                            tabItems[key]?.label()
+                                .contentShape(Rectangle())
+                                .onTapGesture { selection = key }
+                        }
+//                        Text("wheee")
+//
+//                        Text("whooo")
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .background(.regularMaterial)
+            }
+            .onPreferenceChange(FancyTabItemPreferenceKey<Selection>.self) {
+//                print("item keys changed")
+//                print($0)
+                self.tabItemKeys = $0
+                // self.tabItemKeys.append(contentsOf: $0)
+            }
+            .onPreferenceChange(FancyTabItemLabelBuilderPreferenceKey<Selection>.self) {
+                // self.tabItems = self.tabItems.reduce($0)
+                self.tabItems = $0
+                // self.tabItems.merge($0, uniquingKeysWith: { $1 })
+            }
+            .environment(\.tabSelectionHashValue, selection.hashValue)
+    }
+}
