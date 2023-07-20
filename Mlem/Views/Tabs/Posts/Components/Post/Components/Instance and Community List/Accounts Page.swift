ead621184eca9e5d4f3c0219d5a91643e489e11b
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2023-07-20 22:00:23 +0100

    Bug fix

--- Mlem/Views/Tabs/Posts/Components/Post/Components/Instance and Community List/Accounts Page.swift
+++ Mlem/Views/Tabs/Posts/Components/Post/Components/Instance and Community List/Accounts Page.swift
@@ -14,57 +14,54 @@ struct AccountsPage: View {
     @Environment(\.forceOnboard) var forceOnboard
     
     @State private var isShowingInstanceAdditionSheet: Bool = false
-    @State var selectedAccount: SavedAccount?
+    @Binding var selectedAccount: SavedAccount?
     
     let onboarding: Bool
+    
+    init(selectedAccount: Binding<SavedAccount?> = Binding.constant(nil),
+         onboarding: Bool = false) {
+        self._selectedAccount = selectedAccount
+        self.onboarding = onboarding
+    }
     
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
         let instances = Array(accountsTracker.accountsByInstance.keys)
         
-        Group {
-            if onboarding || instances.isEmpty || isShowingInstanceAdditionSheet {
-                AddSavedInstanceView(onboarding: onboarding,
-                                     currentAccount: $selectedAccount)
-            } else {
-                List {
-                    ForEach(instances, id: \.self) { instance in
-                        Section(header: Text(instance)) {
-                            ForEach(accountsTracker.accountsByInstance[instance] ?? []) { account in
-                                Button(account.username) {
-                                    dismiss()
-                                    
-                                    // this tiny delay prevents the modal dismiss animation from being cancelled
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
-                                        appState.setActiveAccount(account)
-                                    }
+        if onboarding || instances.isEmpty || isShowingInstanceAdditionSheet {
+            AddSavedInstanceView(onboarding: onboarding,
+                                 currentAccount: $selectedAccount)
+        } else {
+            List {
+                ForEach(instances, id: \.self) { instance in
+                    Section(header: Text(instance)) {
+                        ForEach(accountsTracker.accountsByInstance[instance] ?? []) { account in
+                            Button(account.username) {
+                                dismiss()
+                                
+                                // this tiny delay prevents the modal dismiss animation from being cancelled
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                                    appState.setActiveAccount(account)
                                 }
-                                .swipeActions {
-                                    Button("Delete", role: .destructive) {
-                                        accountsTracker.removeAccount(account: account, appState: appState, forceOnboard: forceOnboard)
-                                    }
-                                }
-                                .foregroundColor(appState.currentActiveAccount == account ? .secondary : .primary)
                             }
+                            .swipeActions {
+                                Button("Delete", role: .destructive) {
+                                    accountsTracker.removeAccount(account: account, appState: appState, forceOnboard: forceOnboard)
+                                }
+                            }
+                            .foregroundColor(appState.currentActiveAccount == account ? .secondary : .primary)
                         }
                     }
-                    
-                    Button {
-                        isShowingInstanceAdditionSheet = true
-                    } label: {
-                        Text("Add Account")
-                    }
-                    .accessibilityLabel("Add a new account.")
                 }
+                
+                Button {
+                    isShowingInstanceAdditionSheet = true
+                } label: {
+                    Text("Add Account")
+                }
+                .accessibilityLabel("Add a new account.")
             }
-        }
-        .onAppear {
-            self.selectedAccount = appState.currentActiveAccount
-        }
-        .onChange(of: selectedAccount) { account in
-            guard let account else { return }
-            appState.setActiveAccount(account)
         }
     }
 }
