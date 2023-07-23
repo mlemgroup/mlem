56b4eca649458256c394fb1a54090eb6eef7f2d0
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2023-07-23 09:39:15 +0100

    NSFW toggle changes (#354)

--- Mlem/Views/Shared/Posts/Components/NSFW Tag.swift
+++ Mlem/Views/Shared/Posts/Components/NSFW Tag.swift
@@ -24,36 +24,3 @@ struct NSFWTag: View {
             .font((compact ? Font.caption2 : Font.subheadline).weight(compact ? Font.Weight.heavy : Font.Weight.black))
     }
 }
-
-struct NSFWToggle: View {
-    let compact: Bool
-    @Binding var isEnabled: Bool
-    
-    var body: some View {
-        Text("NSFW")
-            .dynamicTypeSize(.small ... .accessibility2)
-            .foregroundColor(isEnabled ? .white : .gray.opacity(0.6))
-            .padding(2)
-            .background(RoundedRectangle(cornerRadius: AppConstants.smallItemCornerRadius)
-                .foregroundColor(isEnabled ? .red : .white))
-            .font((compact ? Font.caption : Font.subheadline).weight(Font.Weight.black))
-            .accessibilityAddTraits(.isButton)
-            .accessibilityLabel("NSFW Toggle, \(isEnabled ? "Enabled" : "Disabled")")
-            .accessibilityHint("Activate to toggle")
-            .onTapGesture {
-                isEnabled.toggle()
-            }
-    }
-}
-
-struct NSFWPreviews: PreviewProvider {
-    static var previews: some View {
-        NSFWTag(compact: false)
-        NSFWTag(compact: true)
-        NSFWToggle(compact: true, isEnabled: Binding.constant(true))
-        NSFWToggle(compact: true, isEnabled: Binding.constant(false))
-        NSFWToggle(compact: false, isEnabled: Binding.constant(true))
-        NSFWToggle(compact: false, isEnabled: Binding.constant(false))
-
-    }
-}
