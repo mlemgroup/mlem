aefbf7259cbef0dbf48bafb18b7213228b235f1c
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-07-28 11:09:47 -0400

    removed unnecessary isLoading from comment tracker

--- Mlem/Models/Trackers/Comment Tracker.swift
+++ Mlem/Models/Trackers/Comment Tracker.swift
@@ -9,7 +9,6 @@ import Foundation
 
 class CommentTracker: ObservableObject {
     @Published var comments: [HierarchicalComment] = .init()
-    @Published var isLoading: Bool = true
     
     private var ids: Set<Int> = .init()
     
