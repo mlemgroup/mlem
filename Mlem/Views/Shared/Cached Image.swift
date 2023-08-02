5689fea5efb84a51071fc9f108e8a9e2b99e6358
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-08-01 20:37:25 -0400

    tidying

--- Mlem/Views/Shared/Cached Image.swift
+++ Mlem/Views/Shared/Cached Image.swift
@@ -13,12 +13,12 @@ import NukeUI
 import Nuke
 
 struct CachedImage: View {
-
+    
     let url: URL?
     let shouldExpand: Bool
     @State var bigPicMode: URL?
     let size: CGSize
-
+    
     init(url: URL?,
          shouldExpand: Bool = true,
          maxHeight: CGFloat = .infinity) {
@@ -36,60 +36,59 @@ struct CachedImage: View {
     }
     
     var body: some View {
-        // GeometryReader { geo in
-            LazyImage(url: url) { state in
-                if let image = state.imageContainer {
-                    let imageView = Image(uiImage: image.image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: size.width, height: size.height)
-                        .clipped()
-                        .allowsHitTesting(false)
-                        .overlay(alignment: .top) {
-                            // weeps in janky hack but this lets us tap the image only in the area we want
-                            Rectangle()
-                                .frame(maxHeight: size.height)
-                                .opacity(0.00000000001)
-                        }
-                    if shouldExpand {
-                        imageView
-                            .onTapGesture {
-                                Task(priority: .userInitiated) {
-                                    do {
-                                        let (data, _) = try await ImagePipeline.shared.data(for: url!)
-                                        let fileType = url?.pathExtension ?? "png"
-                                        let quicklook = FileManager.default.temporaryDirectory.appending(path: "quicklook.\(fileType)")
-                                        if FileManager.default.fileExists(atPath: quicklook.absoluteString) {
-                                            print("file exsists")
-                                            try FileManager.default.removeItem(at: quicklook)
-                                        }
-                                        try data.write(to: quicklook)
-                                        await MainActor.run {
-                                            bigPicMode = quicklook
-                                        }
-                                    } catch {
-                                        print(String(describing: error))
+        LazyImage(url: url) { state in
+            if let image = state.imageContainer {
+                let imageView = Image(uiImage: image.image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size.width, height: size.height)
+                    .clipped()
+                    .allowsHitTesting(false)
+                    .overlay(alignment: .top) {
+                        // weeps in janky hack but this lets us tap the image only in the area we want
+                        Rectangle()
+                            .frame(maxHeight: size.height)
+                            .opacity(0.00000000001)
+                    }
+                if shouldExpand {
+                    imageView
+                        .onTapGesture {
+                            Task(priority: .userInitiated) {
+                                do {
+                                    let (data, _) = try await ImagePipeline.shared.data(for: url!)
+                                    let fileType = url?.pathExtension ?? "png"
+                                    let quicklook = FileManager.default.temporaryDirectory.appending(path: "quicklook.\(fileType)")
+                                    if FileManager.default.fileExists(atPath: quicklook.absoluteString) {
+                                        print("file exsists")
+                                        try FileManager.default.removeItem(at: quicklook)
                                     }
+                                    try data.write(to: quicklook)
+                                    await MainActor.run {
+                                        bigPicMode = quicklook
+                                    }
+                                } catch {
+                                    print(String(describing: error))
                                 }
                             }
-                            .quickLookPreview($bigPicMode)
-                    } else {
-                        imageView
-                    }
-                } else if state.error != nil {
-                    // Indicates an error
-                    imageNotFound()
-                        .frame(width: size.width, height: size.height)
-                        .background(Color(uiColor: .systemGray4))
-                        .foregroundColor(.secondary)
-                        .cornerRadius(AppConstants.smallItemCornerRadius)
+                        }
+                        .quickLookPreview($bigPicMode)
                 } else {
-                    ProgressView() // Acts as a placeholder
-                        .frame(width: size.width, height: size.height)
+                    imageView
                 }
+            } else if state.error != nil {
+                // Indicates an error
+                imageNotFound()
+                    .frame(width: size.width, height: size.height)
+                    .background(Color(uiColor: .systemGray4))
+                    .foregroundColor(.secondary)
+                    .cornerRadius(AppConstants.smallItemCornerRadius)
+            } else {
+                ProgressView() // Acts as a placeholder
+                    .frame(width: size.width, height: size.height)
             }
-            .processors([.resize(size: size)])
-            .frame(width: size.width, height: size.height)
+        }
+        .processors([.resize(size: size)])
+        .frame(width: size.width, height: size.height)
     }
     
     func imageNotFound() -> some View {
