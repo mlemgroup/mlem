7580394b11a61668beb50a3bbb8952706a62618b
Bosco Ho <boscojwho@gmail.com>
2023-08-04 18:04:04 -0700

    - Remove some personal preference values.

--- Mlem/Animations/Comments.swift
+++ Mlem/Animations/Comments.swift
@@ -12,10 +12,6 @@ internal extension Animation {
     /// Animation for expanding or collapsing a comment and its child comments.
     static func showHideComment() -> Animation {
         let standard = (0.4, 1.0, 0.25)
-        /// I like this =)
-//        let quick = (0.2, 1.0, 0.25)
-        /// Even nicer =P
-//        let quick = (0.225, 1.0, 0.275)
         let animationValues = standard
         return .interactiveSpring(
             response: animationValues.0,
@@ -31,15 +27,6 @@ internal extension AnyTransition {
     }
     
     static func commentView() -> AnyTransition {
-        .asymmetric(
-            insertion: .move(edge: .bottom).combined(with: .opacity),
-            removal: .move(edge: .top).combined(with: .opacity)
-        )
-        
-        // I like this: Parent comment has simple opacity collapse animation, while child comments slide out to the side.
-//        .asymmetric(
-//            insertion: .move(edge: .leading),
-//            removal: .move(edge: .trailing)
-//        )
+        .move(edge: .top).combined(with: .opacity)
     }
 }
