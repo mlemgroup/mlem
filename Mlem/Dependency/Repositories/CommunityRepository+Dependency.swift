963c4ae6023fa562dd420f3e3991ad126af136c4
mormaer <nick@mormaer.dev>
2023-08-20 09:38:18 +0100

    provide `.previewValue` and `.testValue` for `CommunityRepository`

--- Mlem/Dependency/Repositories/CommunityRepository+Dependency.swift
+++ Mlem/Dependency/Repositories/CommunityRepository+Dependency.swift
@@ -7,9 +7,96 @@
 //
 
 import Dependencies
+import Foundation
+import XCTestDynamicOverlay
 
 extension CommunityRepository: DependencyKey {
-  static let liveValue = CommunityRepository()
+    static var liveValue = CommunityRepository()
+    
+    static var previewValue = CommunityRepository(
+        subscriptions: { _ in
+            ["Science", "Technology", "World News", "Music", "Memes", "Aww", "Gaming"]
+                .enumerated()
+                .map { index, name -> APICommunityView in
+                        .mock(
+                            community: .mock(
+                                id: index,
+                                name: name
+                            ),
+                            subscribed: .subscribed
+                        )
+                }
+        },
+        details: { _, id in
+                .mock(
+                    communityView: .mock(
+                        community: .mock(
+                            id: id
+                        )
+                    )
+                )
+        },
+        updateSubscription: { _, id, subscribed in
+                .mock(
+                    community: .mock(
+                        id: id
+                    ),
+                    subscribed: subscribed ? .subscribed : .notSubscribed,
+                    blocked: false,
+                    counts: .mock()
+                )
+        },
+        hideCommunity: { _, id, _ in
+                .mock(
+                    communityView: .mock(
+                        community: .mock(
+                            id: id,
+                            hidden: true
+                        )
+                    )
+                )
+        },
+        unhideCommunity: { _, id in
+                .mock(
+                    communityView: .mock(
+                        community: .mock(
+                            id: id,
+                            hidden: false
+                        )
+                    )
+                )
+        },
+        blockCommunity: { _, id in
+                .mock(
+                    communityView: .mock(
+                        community: .mock(
+                            id: id
+                        )
+                    ),
+                    blocked: true
+                )
+        },
+        unblockCommunity: { _, id in
+                .mock(
+                    communityView: .mock(
+                        community: .mock(
+                            id: id
+                        )
+                    ),
+                    blocked: false
+                )
+        }
+    )
+    
+    static var testValue = CommunityRepository(
+        subscriptions: unimplemented("CommunityRepository.subscriptions"),
+        details: unimplemented("CommunityRepository.details"),
+        updateSubscription: unimplemented("CommunityRepository.updateSubscription"),
+        hideCommunity: unimplemented("CommunityRepository.hideCommunity"),
+        unhideCommunity: unimplemented("CommunityRepository.unhideCommunity"),
+        blockCommunity: unimplemented("CommunityRepository.blockCommunity"),
+        unblockCommunity: unimplemented("CommunityRepository.unblockCommunity")
+    )
 }
 
 extension DependencyValues {
