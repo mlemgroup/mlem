d3a1466143b9e55d0bc13791336e4c66ca575112
mormaer <nick@mormaer.dev>
2023-08-20 09:34:36 +0100

    add extensions for creating `mock` values of objects

---
+++ Mlem/Extensions/Mocks/APICommunityView+Mock.swift
@@ -0,0 +1,25 @@
+// 
+//  APICommunityView+Mock.swift
+//  Mlem
+//
+//  Created by mormaer on 20/08/2023.
+//  
+//
+
+import Foundation
+
+extension APICommunityView {
+    static func mock(
+        community: APICommunity = .mock(),
+        subscribed: APISubscribedStatus = .notSubscribed,
+        blocked: Bool = false,
+        counts: APICommunityAggregates = .mock()
+    ) -> APICommunityView {
+        .init(
+            community: community,
+            subscribed: subscribed,
+            blocked: blocked,
+            counts: counts
+        )
+    }
+}
