5a7c528c831422398c620f1c0e90973f53c5b718
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-08-23 22:47:36 -0400

    added link indicator to thumbnail

---
+++ Mlem/Views/Shared/Components/Components/Website Indicator View.swift
@@ -0,0 +1,24 @@
+//
+//  Website Indicator View.swift
+//  Mlem
+//
+//  Created by Eric Andrews on 2023-08-23.
+//
+
+import Foundation
+import SwiftUI
+
+struct WebsiteIndicatorView: View {
+    var body: some View {
+        Circle()
+            .background(.ultraThinMaterial)
+            .clipShape(Circle())
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay {
+                Image(systemName: "safari")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.white)
+            }
+    }
+}
