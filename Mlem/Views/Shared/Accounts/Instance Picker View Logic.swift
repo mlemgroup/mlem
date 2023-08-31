9f289c18c202e02c983029b418291cd0ef828d80
mormaer <nick@mormaer.dev>
2023-08-31 17:58:09 +0100

    refactor parser to not rely on structure / indexes directly

--- Mlem/Views/Shared/Accounts/Instance Picker View Logic.swift
+++ Mlem/Views/Shared/Accounts/Instance Picker View Logic.swift
@@ -37,71 +37,12 @@ extension InstancePickerView {
         if let url = URL(string: "https://raw.githubusercontent.com/maltfield/awesome-lemmy-instances/main/awesome-lemmy-instances.csv") {
             do {
                 let (data, _) = try await URLSession.shared.data(from: url)
-                if let dataString = String(data: data, encoding: .utf8) {
-                    // split by newlines and remove header row
-                    var splitData: [Substring] = dataString.split(separator: "\n")
-                    
-                    // ensure this is the data we think it is
-                    guard splitData.removeFirst() == "Instance,NU,NC,Fed,Adult,↓V,Users,BI,BB,UT,Version" else {
-                        print("Unexpected header line")
-                        return nil
-                    }
-                    
-                    // map lines to InstanceMetadata structs
-                    let ret: [InstanceMetadata] = splitData
-                        .compactMap { line in
-                            let ret = parseInstanceMetadata(from: line)
-                            if ret == nil { print("Failed to parse line: \(line)") }
-                            return ret
-                        }
-                    
-                    // if found some instances, return them
-                    return ret.count > 0 ? ret : nil
-                }
+                return try InstanceMetadataParser.parse(from: data)
             } catch {
                 errorHandler.handle(error)
             }
         }
         
         return nil
-    }
-    
-    /**
-     Parses a CSV line into instance metadata
-     */
-    private func parseInstanceMetadata(from line: Substring) -> InstanceMetadata? {
-        let fields = line.split(separator: ",")
-        guard fields.count == 11 else { return nil }
-        
-        // matches [instance name](instance url)
-        guard let urlMatch = fields[0].firstMatch(of: /\[(?'name'.*)\]\((?'url'.*)\)/) else { return nil }
-
-        let name = String(urlMatch.output.name)
-        guard let url = URL(string: String(urlMatch.output.url)) else { return nil }
-        let newUsers = fields[1] == "Yes"
-        let newCommunities = fields[2] == "Yes"
-        let federated = fields[3] == "Yes"
-        let adult = fields[4] == "Yes"
-        let downvotes = fields[5] == "Yes"
-        guard let users = Int(fields[6]) else { return nil }
-        guard let blocking = Int(fields[7]) else { return nil }
-        guard let blockedBy = Int(fields[8]) else { return nil }
-        let uptime = String(fields[9])
-        let version = String(fields[10])
-        
-        return InstanceMetadata(
-            name: name,
-            url: url,
-            newUsers: newUsers,
-            newCommunities: newCommunities,
-            federated: federated,
-            adult: adult,
-            downvotes: downvotes,
-            users: users,
-            blocking: blocking,
-            blockedBy: blockedBy,
-            uptime: uptime,
-            version: version
-        )
     }
 }
