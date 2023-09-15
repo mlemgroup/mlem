1ab70880eb4f8b5e10c4a60d10f2eeb98cc7d787
mormaer <mormaer@users.noreply.github.com>
2023-09-15 16:51:34 +0100

    fix - use separate formatters for parsing inconsistent dates from API (#603)

--- Mlem/Extensions/JSONDecoder+Default.swift
+++ Mlem/Extensions/JSONDecoder+Default.swift
@@ -11,35 +11,34 @@ extension JSONDecoder {
     static var defaultDecoder: JSONDecoder {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-
-        let formatter = DateFormatter()
-
-        formatter.timeZone = .gmt
-        formatter.locale = Locale(identifier: "en_US_POSIX")
-
+        
         let formats = [
+            "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ",
             "yyyy-MM-dd'T'HH:mm:ss.SSSSSS",
-            "yyyy-MM-dd'T'HH:mm:ss.SSSSZ",
-            "yyyy-MM-dd'T'HH:mm:ss.SSSS",
-            "yyyy-MM-dd'T'HH:mm:ss",
-            "yyyy-MM-dd HH:mm:ss.SSSSSS",
-            "yyyy-MM-dd HH:mm:ss",
-            "yyyy-MM-dd"
+            "yyyy-MM-dd'T'HH:mm:ssZ",
+            "yyyy-MM-dd'T'HH:mm:ss"
         ]
+        
+        let formatters = formats.map { format in
+            let formatter = DateFormatter()
+            formatter.timeZone = .gmt
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.dateFormat = format
+            return formatter
+        }
 
         decoder.dateDecodingStrategy = .custom({ decoder in
             let container = try decoder.singleValueContainer()
             let string = try container.decode(String.self)
 
-            for format in formats {
-                formatter.dateFormat = format
+            for formatter in formatters {
                 if let date = formatter.date(from: string) {
                     return date
                 }
             }
 
             // after some discussion we've agreed to fail the modelling if the date
-            // does match either of the above, as based on the current API source code
+            // does match _any_ of the above, as based on the current API source code
             // it should be one of those
             throw Swift.DecodingError.dataCorrupted(
                 .init(codingPath: container.codingPath,
