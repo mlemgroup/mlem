25fd82c940a5382378eae20cd79201754ffedb53
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-10-30 23:44:07 -0400

    added new default icon, organized icons

--- Mlem/Extensions/AlternativeIconCell.swift
+++ Mlem/Extensions/AlternativeIconCell.swift
@@ -17,46 +17,8 @@ struct AlternativeIconCell: View {
                 await setAppIcon(icon.id)
             }
         } label: {
-            HStack {
-                getImage()
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: AppConstants.appIconSize, height: AppConstants.appIconSize)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(AppConstants.appIconCornerRadius)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: AppConstants.appIconCornerRadius)
-                            .stroke(Color(.secondarySystemBackground), lineWidth: 1)
-                    }
-                VStack(alignment: .leading) {
-                    Text(icon.name)
-                    if let author = icon.author {
-                        Text(author)
-                            .font(.footnote)
-                            .foregroundColor(.secondary)
-                    }
-                }
-                Spacer()
-                if icon.selected {
-                    Image(systemName: Icons.success)
-                }
-            }
+            AlternativeIconLabel(icon: icon)
         }.accessibilityElement(children: .combine)
-    }
-
-    func getImage() -> Image {
-        let image = {
-            guard let id = icon.id else {
-                return Bundle.main.iconFileName
-                    .flatMap { UIImage(named: $0) }
-                    .map {
-                        Image(uiImage: $0)
-                    } ?? Image(systemName: Icons.noFile)
-            }
-            return Image(uiImage: UIImage(named: id) ?? UIImage(imageLiteralResourceName: id))
-        }
-  
-        return image()
     }
 }
 
