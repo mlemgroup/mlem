1986b4c3dfe73dd2cfc1df8e65b34ebaea097b35
Bosco Ho <boscojwho@gmail.com>
2023-12-13 02:44:09 -0800

    - Fix missing scroll view reader in Profile tab.

--- Mlem/Views/Tabs/Profile/Profile View.swift
+++ Mlem/Views/Tabs/Profile/Profile View.swift
@@ -18,14 +18,17 @@ struct ProfileView: View {
     @StateObject private var navigation: Navigation = .init()
     
     var body: some View {
-        NavigationStack(path: $profileTabNavigation.path) {
-            UserView(userID: userID)
-                .handleLemmyViews()
-                .environmentObject(profileTabNavigation)
-                .tabBarNavigationEnabled(.profile, navigation)
+        ScrollViewReader { proxy in
+            NavigationStack(path: $profileTabNavigation.path) {
+                UserView(userID: userID)
+                    .handleLemmyViews()
+                    .environmentObject(profileTabNavigation)
+                    .tabBarNavigationEnabled(.profile, navigation)
+            }
+            .handleLemmyLinkResolution(navigationPath: .constant(profileTabNavigation))
+            .environment(\.navigationPathWithRoutes, $profileTabNavigation.path)
+            .environment(\.scrollViewProxy, proxy)
+            .environmentObject(navigation)
         }
-        .handleLemmyLinkResolution(navigationPath: .constant(profileTabNavigation))
-        .environment(\.navigationPathWithRoutes, $profileTabNavigation.path)
-        .environmentObject(navigation)
     }
 }
