3ca55929681833e3757ff6c489b357d524ecdc0d
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2023-12-18 20:09:23 -0500

    fixed refresh behaving poorly on 0.19

--- Mlem/Models/Trackers/Post Tracker.swift
+++ Mlem/Models/Trackers/Post Tracker.swift
@@ -136,6 +136,7 @@ class PostTracker: ObservableObject {
         }
         
         page = 1
+        currentCursor = nil
         
         let (newPosts, cursor) = try await postRepository.loadPage(
             communityId: communityId,
