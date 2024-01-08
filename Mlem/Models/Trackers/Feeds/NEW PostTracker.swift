2e4db5d12fdff5f099b68f4f247247cafc3a4a86
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-01-07 20:52:31 -0500

    added most basic navigation back to feeds

--- Mlem/Models/Trackers/Feeds/NEW PostTracker.swift
+++ Mlem/Models/Trackers/Feeds/NEW PostTracker.swift
@@ -9,11 +9,17 @@ import Dependencies
 import Foundation
 
 enum NewFeedType {
-    case all
+    case all, local, subscribed, saved
     
     var toLegacyFeedType: FeedType {
         switch self {
         case .all:
+            return .all
+        case .local:
+            return .local
+        case .subscribed:
+            return .subscribed
+        case .saved:
             return .all
         }
     }
