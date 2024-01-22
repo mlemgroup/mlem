6e39f0da896017b58913073f0abdfcf8ab6b7afe
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-01-22 10:58:59 -0500

    initial work towards post and comment tracker

---
+++ Mlem/Models/Content/CommentModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommentModel.swift
+//  Mlem
+//
+//  Created by Eric Andrews on 2024-01-22.
+//
+
+import Foundation
---
+++ Mlem/Models/Trackers/Feeds/UserContentTracker.swift
@@ -0,0 +1,8 @@
+//
+//  UserContentTracker.swift
+//  Mlem
+//
+//  Created by Eric Andrews on 2024-01-22.
+//
+
+import Foundation
