e73c7a1494f544d4e586f66d7dc6d640f74b48f0
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-01-28 21:57:36 +0000

    Update InstanceModel.swift

--- Mlem/Models/Content/Instance/InstanceModel.swift
+++ Mlem/Models/Content/Instance/InstanceModel.swift
@@ -42,8 +42,6 @@ struct InstanceModel {
     var applicationsEmailAdmins: Bool?
     var reportsEmailAdmins: Bool?
     
-    var slurFilterRegex: Regex<AnyRegexOutput>?
-    
     init(from response: SiteResponse) {
         self.update(with: response)
     }
