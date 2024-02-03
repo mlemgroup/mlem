aa182e80580d842a16e6a42e7f554708767f5501
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-02-03 22:41:20 +0000

    Update

--- Mlem/Models/Content/Post/PostModel.swift
+++ Mlem/Models/Content/Post/PostModel.swift
@@ -44,6 +44,7 @@ class PostModel: ContentIdentifiable, ObservableObject {
         self.creator = UserModel(from: apiPostView.creator)
         self.creator.blocked = apiPostView.creatorBlocked
         self.community = CommunityModel(from: apiPostView.community, subscribed: apiPostView.subscribed.isSubscribed)
+        self.community.blocked = false
         self.votes = VotesModel(from: apiPostView.counts, myVote: apiPostView.myVote)
         self.commentCount = apiPostView.counts.comments
         self.unreadCommentCount = apiPostView.unreadComments
