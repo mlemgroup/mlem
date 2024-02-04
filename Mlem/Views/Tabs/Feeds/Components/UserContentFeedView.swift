e934b9df71cd4bbac94f26ee72a9357791e12136
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-02-04 09:48:45 +0000

    update

--- Mlem/Views/Tabs/Feeds/Components/UserContentFeedView.swift
+++ Mlem/Views/Tabs/Feeds/Components/UserContentFeedView.swift
@@ -120,7 +120,8 @@ struct UserContentFeedView: View {
                     .frame(maxWidth: .infinity)
             } else if userContentTracker.loadingState == .done {
                 // NoPostsView(loadingState: postTracker.loadingState)
-                Text("no items :(")
+                Text("No items :(")
+                    .padding(.top, 20)
                     .transition(.scale(scale: 0.9).combined(with: .opacity))
             }
         }
