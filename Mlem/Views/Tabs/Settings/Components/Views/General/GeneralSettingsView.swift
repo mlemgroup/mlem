089b8125335c6c4d79c77165e91abdb142d8d927
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-02-10 17:49:19 -0500

    added version check to disable mark read on scroll option for instances under 0.19

--- Mlem/Views/Tabs/Settings/Components/Views/General/GeneralSettingsView.swift
+++ Mlem/Views/Tabs/Settings/Components/Views/General/GeneralSettingsView.swift
@@ -65,6 +65,11 @@ struct GeneralSettingsView: View {
                     settingName: "Mark Read on Scroll",
                     isTicked: $markReadOnScroll
                 )
+                .disabled(siteInformation.version ?? .infinity <= .init("0.19.0"))
+            } footer: {
+                if siteInformation.version ?? .infinity <= .init("0.19.0") {
+                    Text("Mark read on scroll is only available on instances running v0.19.0 or greater.")
+                }
             }
             
             Section {
