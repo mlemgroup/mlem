de8ff00c09c57e9f22dda8e552be5bcf3e58b607
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-03-10 15:26:19 +0000

    oops

--- Mlem/Views/Shared/Composer/ResponseEditorView.swift
+++ Mlem/Views/Shared/Composer/ResponseEditorView.swift
@@ -51,6 +51,7 @@ struct ResponseEditorView: View {
                         bodyEditorModel: bodyEditorModel,
                         attachmentModel: attachmentModel
                     )
+                    .linkAttachmentModel(model: attachmentModel)
                     .lineLimit(AppConstants.textFieldVariableLineLimit)
                     .accessibilityLabel("Response Body")
                     .padding(AppConstants.standardSpacing)
