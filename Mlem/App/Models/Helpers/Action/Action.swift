bc47fdf713082545e6a4340f7ae783a2c21a0257
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-03-30 23:05:44 +0000

    update

--- Mlem/App/Models/Helpers/Action/Action.swift
+++ Mlem/App/Models/Helpers/Action/Action.swift
@@ -57,4 +57,43 @@ struct BasicAction: Action {
         self.swipeIcon2 = swipeIcon2
         self.callback = enabled ? callback : nil
     }
+    
+    static func upvote(isOn: Bool) -> BasicAction {
+        .init(
+            type: .upvote,
+            isOn: isOn,
+            label: isOn ? "Undo Upvote" : "Upvote",
+            color: Colors.upvoteColor,
+            barIcon: Icons.upvote,
+            menuIcon: isOn ? Icons.upvoteSquareFill : Icons.upvoteSquare,
+            swipeIcon1: isOn ? Icons.resetVoteSquare : Icons.upvoteSquare,
+            swipeIcon2: isOn ? Icons.resetVoteSquareFill : Icons.upvoteSquareFill
+        )
+    }
+    
+    static func downvote(isOn: Bool) -> BasicAction {
+        .init(
+            type: .downvote,
+            isOn: isOn,
+            label: isOn ? "Undo Downvote" : "Downvote",
+            color: Colors.downvoteColor,
+            barIcon: Icons.downvote,
+            menuIcon: isOn ? Icons.downvoteSquareFill : Icons.downvoteSquare,
+            swipeIcon1: isOn ? Icons.resetVoteSquare : Icons.downvoteSquare,
+            swipeIcon2: isOn ? Icons.resetVoteSquareFill : Icons.downvoteSquareFill
+        )
+    }
+    
+    static func save(isOn: Bool) -> BasicAction {
+        .init(
+            type: .save,
+            isOn: isOn,
+            label: isOn ? "Undo Save": "Save",
+            color: Colors.saveColor,
+            barIcon: isOn ? Icons.saveFill : Icons.save,
+            menuIcon: isOn ? Icons.saveFill : Icons.save,
+            swipeIcon1: isOn ? Icons.unsave : Icons.save,
+            swipeIcon2: isOn ? Icons.unsaveFill : Icons.saveFill
+        )
+    }
 }
