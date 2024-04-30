d0698608e5587f0d8edd8590c0a1dfe90d14a85e
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-04-30 18:48:10 +0100

    Fix tab icon color

--- Mlem/App/Views/Shared/CustomTabBarController.swift
+++ Mlem/App/Views/Shared/CustomTabBarController.swift
@@ -21,15 +21,17 @@ class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
     }
     
     // This is used for Storyboard, and wont ever be called as long as we dont use Storyboard
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.delegate = self
-        self.hidesBottomBarWhenPushed = true
-
+        delegate = self
+        hidesBottomBarWhenPushed = true
+        
+        tabBar.tintColor = .systemBlue
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressGestureTriggered(_:)))
         tabBar.addGestureRecognizer(longPressRecognizer)
         
@@ -42,7 +44,7 @@ class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
         guard recognizer.state == .began else { return }
         guard let tabBar = recognizer.view as? UITabBar else { return }
         guard let tabBarItems = tabBar.items else { return }
-        guard let viewControllers = viewControllers else { return }
+        guard let viewControllers else { return }
         guard tabBarItems.count == viewControllers.count else { return }
 
         let loc = recognizer.location(in: tabBar)
