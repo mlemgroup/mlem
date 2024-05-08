0f3c667bfb4a5506d62bb1a64fd626db2d90c91d
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-05-08 20:05:20 +0100

    Update

--- Mlem/App/Utility/Extensions/Content Models/Interactable1Providing+Extensions.swift
+++ Mlem/App/Utility/Extensions/Content Models/Interactable1Providing+Extensions.swift
@@ -9,15 +9,26 @@ import Foundation
 import MlemMiddleware
 
 extension Interactable1Providing {
+    private var self2: (any Interactable2Providing)? { self as? any Interactable2Providing }
+        
     var upvoteAction: BasicAction {
-        .upvote(isOn: false)
+        .upvote(
+            isOn: (self2?.votes.myVote ?? .none == .upvote),
+            callback: api.willSendToken ? self2?.toggleUpvote : nil
+        )
     }
     
     var downvoteAction: BasicAction {
-        .downvote(isOn: false)
+        .downvote(
+            isOn: (self2?.votes.myVote ?? .none == .downvote),
+            callback: api.willSendToken ? self2?.toggleDownvote : nil
+        )
     }
 
     var saveAction: BasicAction {
-        .save(isOn: false)
+        .save(
+            isOn: self2?.isSaved ?? false,
+            callback: api.willSendToken ? self2?.toggleSave : nil
+        )
     }
 }
