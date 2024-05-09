615c499223779ea665e2e3478de660df2211788a
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-05-09 19:48:01 +0100

    Update

--- Mlem/App/Utility/Extensions/Content Models/Interactable1Providing+Extensions.swift
+++ Mlem/App/Utility/Extensions/Content Models/Interactable1Providing+Extensions.swift
@@ -12,22 +12,43 @@ extension Interactable1Providing {
     private var self2: (any Interactable2Providing)? { self as? any Interactable2Providing }
         
     var upvoteAction: BasicAction {
-        .upvote(
-            isOn: (self2?.votes.myVote ?? .none == .upvote),
+        let isOn: Bool = (self2?.votes.myVote ?? .none == .upvote)
+        return .init(
+            isOn: isOn,
+            label: isOn ? "Undo Upvote" : "Upvote",
+            color: Colors.upvoteColor,
+            icon: Icons.upvote,
+            menuIcon: isOn ? Icons.upvoteSquareFill : Icons.upvoteSquare,
+            swipeIcon1: isOn ? Icons.resetVoteSquare : Icons.upvoteSquare,
+            swipeIcon2: isOn ? Icons.resetVoteSquareFill : Icons.upvoteSquareFill,
             callback: api.willSendToken ? self2?.toggleUpvote : nil
         )
     }
     
     var downvoteAction: BasicAction {
-        .downvote(
-            isOn: (self2?.votes.myVote ?? .none == .downvote),
+        let isOn: Bool = (self2?.votes.myVote ?? .none == .downvote)
+        return .init(
+            isOn: isOn,
+            label: isOn ? "Undo Downvote" : "Downvote",
+            color: Colors.downvoteColor,
+            icon: Icons.downvote,
+            menuIcon: isOn ? Icons.downvoteSquareFill : Icons.downvoteSquare,
+            swipeIcon1: isOn ? Icons.resetVoteSquare : Icons.downvoteSquare,
+            swipeIcon2: isOn ? Icons.resetVoteSquareFill : Icons.downvoteSquareFill,
             callback: api.willSendToken ? self2?.toggleDownvote : nil
         )
     }
 
     var saveAction: BasicAction {
-        .save(
-            isOn: self2?.isSaved ?? false,
+        let isOn: Bool = self2?.isSaved ?? false
+        return .init(
+            isOn: isOn,
+            label: isOn ? "Unsave" : "Save",
+            color: Colors.saveColor,
+            icon: isOn ? Icons.saveFill : Icons.save,
+            menuIcon: isOn ? Icons.saveFill : Icons.save,
+            swipeIcon1: isOn ? Icons.unsave : Icons.save,
+            swipeIcon2: isOn ? Icons.unsaveFill : Icons.saveFill,
             callback: api.willSendToken ? self2?.toggleSave : nil
         )
     }
