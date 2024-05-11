e88e7677b1ab7ab2bebde62b013a4f60d7be22af
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-05-11 13:40:35 +0100

    Update

--- Mlem/App/Utility/Extensions/Content Models/Community1Providing+Extensions.swift
+++ Mlem/App/Utility/Extensions/Content Models/Community1Providing+Extensions.swift
@@ -9,11 +9,34 @@ import Foundation
 import MlemMiddleware
 
 extension Community1Providing {
+    private var self2: (any Community2Providing)? { self as? any Community2Providing }
+    
     var subscribeAction: BasicAction {
-        .subscribe(isOn: false)
+        let isOn: Bool = self2?.subscribed ?? false
+        return .init(
+            isOn: isOn,
+            label: isOn ? "Unsubscribe" : "Subscribe",
+            color: isOn ? .green : .red,
+            icon: isOn ? Icons.unsubscribe : Icons.subscribe,
+            barIcon: Icons.subscribe,
+            swipeIcon1: isOn ? Icons.unsubscribePerson : Icons.subscribePerson,
+            swipeIcon2: isOn ? Icons.unsubscribePersonFill : Icons.subscribePersonFill,
+            callback: api.willSendToken ? self2?.toggleSubscribe : nil
+        )
     }
     
     var favoriteAction: BasicAction {
-        .favorite(isOn: false)
+        let isOn: Bool = self2?.favorited ?? false
+        return .init(
+            isOn: isOn,
+            label: isOn ? "Unfavorite" : "Favorite",
+            color: .blue,
+            icon: isOn ? Icons.unfavorite : Icons.favorite,
+            barIcon: Icons.favorite,
+            menuIcon: isOn ? Icons.favoriteFill : Icons.favorite,
+            swipeIcon1: isOn ? Icons.unfavorite : Icons.favorite,
+            swipeIcon2: isOn ? Icons.unfavoriteFill : Icons.favoriteFill,
+            callback: api.willSendToken ? self2?.toggleFavorite : nil
+        )
     }
 }
