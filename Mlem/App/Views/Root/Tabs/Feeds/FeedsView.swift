962c1cbb2cbf6514662f28ae956027b7a154a6e0
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-05-17 17:07:27 -0400

    remove test link

--- Mlem/App/Views/Root/Tabs/Feeds/FeedsView.swift
+++ Mlem/App/Views/Root/Tabs/Feeds/FeedsView.swift
@@ -118,8 +118,7 @@ struct MinimalPostFeedView: View {
                     .id(scrollToTop)
                 
                 ForEach(postTracker.items, id: \.uid) { post in
-                    // NavigationLink(value: NavigationPage.expandedPost(post)) {
-                    NavigationLink(value: NavigationPage.expandedPost(PostStub(api: post.api, actorId: post.actorId))) {
+                    NavigationLink(value: NavigationPage.expandedPost(post)) {
                         HStack {
                             actionButton(post.upvoteAction)
                             actionButton(post.downvoteAction)
