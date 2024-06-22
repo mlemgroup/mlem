41a28b13ca72dd7bb497f0e1c9437ad09d476523
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-06-21 22:42:43 -0400

    unified FeedPostView

--- Mlem/App/Views/Root/Tabs/Feeds/FeedsView.swift
+++ Mlem/App/Views/Root/Tabs/Feeds/FeedsView.swift
@@ -138,8 +138,7 @@ struct MinimalPostFeedView: View {
                         if !post.read || showRead {
                             VStack(spacing: 0) { // this improves performance O_o
                                 NavigationLink(value: NavigationPage.expandedPost(post)) {
-                                    PostView(post: post)
-                                        .contentShape(.rect)
+                                    FeedPostView(post: post)
                                 }
                                 .buttonStyle(EmptyButtonStyle())
                                 if !tilePosts { Divider() }
