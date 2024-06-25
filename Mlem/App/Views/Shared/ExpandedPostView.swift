e5ebdcc9bb434803ccb889847933a793ae4117d6
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-06-25 10:03:22 +0100

    Update ExpandedPostView.swift

--- Mlem/App/Views/Shared/ExpandedPostView.swift
+++ Mlem/App/Views/Shared/ExpandedPostView.swift
@@ -13,16 +13,37 @@ struct ExpandedPostView: View {
     @Environment(\.dismiss) var dismiss
     
     let post: AnyPost
+    @State var comments: [Comment2] = []
+    @State var loadingState: LoadingState = .idle
     
     var body: some View {
         ContentLoader(model: post) { post1, _ in
             content(for: post1)
+                .task {
+                    guard loadingState == .idle else { return }
+                    loadingState = .loading
+                    do {
+                        comments = try await post1.getComments(page: 1, limit: 3)
+                        loadingState = .done
+                    } catch {
+                        handleError(error)
+                    }
+                }
         }
     }
     
     func content(for post: any Post1Providing) -> some View {
         FancyScrollView {
-            LargePostView(post: post, isExpanded: true)
+            LazyVStack(alignment: .leading) {
+                LargePostView(post: post, isExpanded: true)
+                Divider()
+                ForEach(comments) { comment in
+                    VStack(alignment: .leading) {
+                        Text(comment.content)
+                        Divider()
+                    }
+                }
+            }
         }
     }
 }
