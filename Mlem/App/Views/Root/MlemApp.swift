116ef36d3e524252ec0825a3a32980781ca4d9e5
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-07-04 15:03:43 +0100

    Update MlemApp.swift

--- Mlem/App/Views/Root/MlemApp.swift
+++ Mlem/App/Views/Root/MlemApp.swift
@@ -5,14 +5,35 @@
 //  Created by Eric Andrews on 2024-02-21.
 //
 
+import Nuke
 import SwiftUI
 
 /// Root view for the app
 @main
 struct MlemApp: App {
+    init() {
+        var imageConfig = ImagePipeline.Configuration.withDataCache(name: "main", sizeLimit: AppConstants.cacheSize)
+        imageConfig.dataLoadingQueue = OperationQueue(maxConcurrentCount: 8)
+        imageConfig.imageDecodingQueue = OperationQueue(maxConcurrentCount: 8) // Let's use those CORES
+        imageConfig.imageDecompressingQueue = OperationQueue(maxConcurrentCount: 8)
+        
+        // I'm leaving that here for mormaer, once I get a handle on rate limites that's where we put em!
+        // imageConfig.isRateLimiterEnabled
+        ImagePipeline.shared = ImagePipeline(configuration: imageConfig)
+        
+        URLCache.shared = AppConstants.urlCache
+    }
+    
     var body: some Scene {
         WindowGroup {
             FlowRoot()
         }
+    }
+}
+
+extension OperationQueue {
+    convenience init(maxConcurrentCount: Int) {
+        self.init()
+        self.maxConcurrentOperationCount = maxConcurrentCount
     }
 }
