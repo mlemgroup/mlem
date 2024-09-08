6c51de49a3bfcb2f42cdf4d376d454ecf795cc6b
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-09-08 14:16:40 -0400

    optional approach

--- Mlem/App/Configuration/User Settings/CodableSettings.swift
+++ Mlem/App/Configuration/User Settings/CodableSettings.swift
@@ -9,80 +9,85 @@ import Foundation
 import MlemMiddleware
 import UIKit
 
+/// Mirror of Settings but without any AppStorage complexity and fully optionalized. This is not pretty but prevents
 struct CodableSettings: Codable {
-    var postSize: PostSize
-    var defaultPostSort: ApiSortType
-    var fallbackPostSort: ApiSortType
-    var thumbnailLocation: ThumbnailLocation
-    var showPostCreator: Bool
+    var postSize: PostSize?
+    var defaultPostSort: ApiSortType?
+    var fallbackPostSort: ApiSortType?
+    var thumbnailLocation: ThumbnailLocation?
+    var showPostCreator: Bool?
     
-    var quickSwipesEnabled: Bool
+    var quickSwipesEnabled: Bool?
     
-    var hapticLevel: HapticPriority
-    var upvoteOnSave: Bool = false
-    var internetSpeed: InternetSpeed
+    var hapticLevel: HapticPriority?
+    var upvoteOnSave: Bool?
+    var internetSpeed: InternetSpeed?
     
-    var keepPlaceOnAccountSwitch: Bool
-    var accountSort: AccountSortMode
-    var groupAccountSort: Bool
+    var keepPlaceOnAccountSwitch: Bool?
+    var accountSort: AccountSortMode?
+    var groupAccountSort: Bool?
     
-    var interfaceStyle: UIUserInterfaceStyle
-    var colorPalette: PaletteOption
+    var interfaceStyle: UIUserInterfaceStyle?
+    var colorPalette: PaletteOption?
     
-    var developerMode: Bool
+    var developerMode: Bool?
     
-    var blurNsfw: NsfwBlurBehavior
-    var showNsfwCommunityWarning: Bool
+    var blurNsfw: NsfwBlurBehavior?
+    var showNsfwCommunityWarning: Bool?
     
-    var openLinksInBrowser: Bool
-    var openLinksInReaderMode: Bool
+    var openLinksInBrowser: Bool?
+    var openLinksInReaderMode: Bool?
     
-    var markReadOnScroll: Bool
-    var showReadInFeed: Bool
-    var defaultFeed: FeedSelection
+    var markReadOnScroll: Bool?
+    var showReadInFeed: Bool?
+    var defaultFeed: FeedSelection?
     
-    var showReadInInbox: Bool
+    var showReadInInbox: Bool?
     
-    var subscriptionInstanceLocation: InstanceLocation
+    var subscriptionInstanceLocation: InstanceLocation?
     
-    var subscriptionSort: SubscriptionListSort
+    var subscriptionSort: SubscriptionListSort?
     
-    var showPersonAvatar: Bool
+    var showPersonAvatar: Bool?
     
-    var showCommunityAvatar: Bool
+    var showCommunityAvatar: Bool?
     
-    var jumpButton: CommentJumpButtonLocation
-    var commentSort: ApiCommentSortType
+    var compactComments: Bool?
+    var jumpButton: CommentJumpButtonLocation?
+    var commentSort: ApiCommentSortType?
+    
+    override func decode
     
     init(from settings: Settings) {
-        self.postSize = settings.postSize
-        self.defaultPostSort = settings.defaultPostSort
-        self.fallbackPostSort = settings.fallbackPostSort
-        self.thumbnailLocation = settings.thumbnailLocation
-        self.showPostCreator = settings.showPostCreator
-        self.quickSwipesEnabled = settings.quickSwipesEnabled
-        self.hapticLevel = settings.hapticLevel
-        self.upvoteOnSave = settings.upvoteOnSave
-        self.internetSpeed = settings.internetSpeed
-        self.keepPlaceOnAccountSwitch = settings.keepPlaceOnAccountSwitch
-        self.accountSort = settings.accountSort
-        self.groupAccountSort = settings.groupAccountSort
-        self.interfaceStyle = settings.interfaceStyle
-        self.colorPalette = settings.colorPalette
-        self.developerMode = settings.developerMode
-        self.blurNsfw = settings.blurNsfw
-        self.showNsfwCommunityWarning = settings.showNsfwCommunityWarning
-        self.openLinksInBrowser = settings.openLinksInBrowser
-        self.openLinksInReaderMode = settings.openLinksInReaderMode
-        self.markReadOnScroll = settings.markReadOnScroll
-        self.showReadInFeed = settings.showReadInFeed
-        self.defaultFeed = settings.defaultFeed
-        self.showReadInInbox = settings.showReadInInbox
-        self.subscriptionInstanceLocation = settings.subscriptionInstanceLocation
-        self.subscriptionSort = settings.subscriptionSort
-        self.showPersonAvatar = settings.showPersonAvatar
-        self.showCommunityAvatar = settings.showCommunityAvatar
-        self.jumpButton = settings.jumpButton
-        self.commentSort = settings.commentSort
+        postSize = settings.postSize
+        defaultPostSort = settings.defaultPostSort
+        fallbackPostSort = settings.fallbackPostSort
+        thumbnailLocation = settings.thumbnailLocation
+        showPostCreator = settings.showPostCreator
+        quickSwipesEnabled = settings.quickSwipesEnabled
+        hapticLevel = settings.hapticLevel
+        upvoteOnSave = settings.upvoteOnSave
+        internetSpeed = settings.internetSpeed
+        keepPlaceOnAccountSwitch = settings.keepPlaceOnAccountSwitch
+        accountSort = settings.accountSort
+        groupAccountSort = settings.groupAccountSort
+        interfaceStyle = settings.interfaceStyle
+        colorPalette = settings.colorPalette
+        developerMode = settings.developerMode
+        blurNsfw = settings.blurNsfw
+        showNsfwCommunityWarning = settings.showNsfwCommunityWarning
+        openLinksInBrowser = settings.openLinksInBrowser
+        openLinksInReaderMode = settings.openLinksInReaderMode
+        markReadOnScroll = settings.markReadOnScroll
+        showReadInFeed = settings.showReadInFeed
+        defaultFeed = settings.defaultFeed
+        showReadInInbox = settings.showReadInInbox
+        subscriptionInstanceLocation = settings.subscriptionInstanceLocation
+        subscriptionSort = settings.subscriptionSort
+        showPersonAvatar = settings.showPersonAvatar
+        showCommunityAvatar = settings.showCommunityAvatar
+        compactComments = settings.compactComments
+        jumpButton = settings.jumpButton
+        commentSort = settings.commentSort
     }
 }
--- Mlem/App/Configuration/User Settings/Settings.swift
+++ Mlem/App/Configuration/User Settings/Settings.swift
@@ -84,37 +84,38 @@ class Settings: ObservableObject {
         }
     }
     
-    /// Re-initializes all values from the given Settings object. This ensures that AppStorage values get updated canonically.
+    /// Re-initializes all values from the given CodableSettings object. Any null keys in CodableSettings will be ignored.
     @MainActor
     func reinit(from settings: CodableSettings) {
-        postSize = settings.postSize
-        defaultPostSort = settings.defaultPostSort
-        fallbackPostSort = settings.fallbackPostSort
-        thumbnailLocation = settings.thumbnailLocation
-        showPostCreator = settings.showPostCreator
-        quickSwipesEnabled = settings.quickSwipesEnabled
-        hapticLevel = settings.hapticLevel
-        upvoteOnSave = settings.upvoteOnSave
-        internetSpeed = settings.internetSpeed
-        keepPlaceOnAccountSwitch = settings.keepPlaceOnAccountSwitch
-        accountSort = settings.accountSort
-        groupAccountSort = settings.groupAccountSort
-        interfaceStyle = settings.interfaceStyle
-        colorPalette = settings.colorPalette
-        developerMode = settings.developerMode
-        blurNsfw = settings.blurNsfw
-        showNsfwCommunityWarning = settings.showNsfwCommunityWarning
-        openLinksInBrowser = settings.openLinksInBrowser
-        openLinksInReaderMode = settings.openLinksInReaderMode
-        markReadOnScroll = settings.markReadOnScroll
-        showReadInFeed = settings.showReadInFeed
-        defaultFeed = settings.defaultFeed
-        showReadInInbox = settings.showReadInInbox
-        subscriptionInstanceLocation = settings.subscriptionInstanceLocation
-        subscriptionSort = settings.subscriptionSort
-        showPersonAvatar = settings.showPersonAvatar
-        showCommunityAvatar = settings.showCommunityAvatar
-        jumpButton = settings.jumpButton
-        commentSort = settings.commentSort
+        postSize = settings.postSize ?? postSize
+        defaultPostSort = settings.defaultPostSort ?? defaultPostSort
+        fallbackPostSort = settings.fallbackPostSort ?? fallbackPostSort
+        thumbnailLocation = settings.thumbnailLocation ?? thumbnailLocation
+        showPostCreator = settings.showPostCreator ?? showPostCreator
+        quickSwipesEnabled = settings.quickSwipesEnabled ?? quickSwipesEnabled
+        hapticLevel = settings.hapticLevel ?? hapticLevel
+        upvoteOnSave = settings.upvoteOnSave ?? upvoteOnSave
+        internetSpeed = settings.internetSpeed ?? internetSpeed
+        keepPlaceOnAccountSwitch = settings.keepPlaceOnAccountSwitch ?? keepPlaceOnAccountSwitch
+        accountSort = settings.accountSort ?? accountSort
+        groupAccountSort = settings.groupAccountSort ?? groupAccountSort
+        interfaceStyle = settings.interfaceStyle ?? interfaceStyle
+        colorPalette = settings.colorPalette ?? colorPalette
+        developerMode = settings.developerMode ?? developerMode
+        blurNsfw = settings.blurNsfw ?? blurNsfw
+        showNsfwCommunityWarning = settings.showNsfwCommunityWarning ?? showNsfwCommunityWarning
+        openLinksInBrowser = settings.openLinksInBrowser ?? openLinksInBrowser
+        openLinksInReaderMode = settings.openLinksInReaderMode ?? openLinksInReaderMode
+        markReadOnScroll = settings.markReadOnScroll ?? markReadOnScroll
+        showReadInFeed = settings.showReadInFeed ?? showReadInFeed
+        defaultFeed = settings.defaultFeed ?? defaultFeed
+        showReadInInbox = settings.showReadInInbox ?? showReadInInbox
+        subscriptionInstanceLocation = settings.subscriptionInstanceLocation ?? subscriptionInstanceLocation
+        subscriptionSort = settings.subscriptionSort ?? subscriptionSort
+        showPersonAvatar = settings.showPersonAvatar ?? showPersonAvatar
+        showCommunityAvatar = settings.showCommunityAvatar ?? showCommunityAvatar
+        compactComments = settings.compactComments ?? compactComments
+        jumpButton = settings.jumpButton ?? jumpButton
+        commentSort = settings.commentSort ?? commentSort
     }
 }
