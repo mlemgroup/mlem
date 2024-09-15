a1cf8cdaa51de8d4d90bc559f888349a00731454
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-09-15 12:31:32 -0400

    ios 18 mark read on scroll

--- Mlem/App/Utility/Extensions/Views/View Modifiers/View+MarkReadOnScroll.swift
+++ Mlem/App/Utility/Extensions/Views/View Modifiers/View+MarkReadOnScroll.swift
@@ -19,6 +19,26 @@ private struct MarkReadOnScroll: ViewModifier {
     @Binding var bottomAppearedItemIndex: Int
     
     func body(content: Content) -> some View {
+        if #available(iOS 18.0, *) {
+            ios18Body(content: content)
+        } else {
+            legacyBody(content: content)
+        }
+    }
+    
+    @available(iOS 18.0, *)
+    func ios18Body(content: Content) -> some View {
+        content
+            .onGeometryChange(for: Bool.self) { geometry in
+                geometry.frame(in: .global).maxY < 0
+            } action: { wasAboveTop, isAboveTop in
+                if !wasAboveTop, isAboveTop {
+                    post.updateRead(true, shouldQueue: true)
+                }
+            }
+    }
+    
+    func legacyBody(content: Content) -> some View {
         content
             .task {
                 do {
