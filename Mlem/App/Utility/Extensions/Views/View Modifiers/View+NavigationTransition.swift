3a76438d90f2a78ac3388be6586989b5d670a1a6
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-09-16 21:27:55 +0100

    Update View+NavigationTransition.swift

--- Mlem/App/Utility/Extensions/Views/View Modifiers/View+NavigationTransition.swift
+++ Mlem/App/Utility/Extensions/Views/View Modifiers/View+NavigationTransition.swift
@@ -9,24 +9,30 @@ import SwiftUI
 
 extension View {
     func navigationTransition_(sourceID: any Hashable, in namespace: Namespace.ID?) -> some View {
-        Group {
-            if #available(iOS 18.0, *), let namespace {
-                self.navigationTransition(.zoom(sourceID: sourceID, in: namespace))
-            } else {
-                self
-            }
-        }
+        // The code below requires Xcode 16, and is intentionally left commented for now
+        // until we upgrade to Xcode 16.
+        self
+//        Group {
+//            if #available(iOS 18.0, *), let namespace {
+//                self.navigationTransition(.zoom(sourceID: sourceID, in: namespace))
+//            } else {
+//                self
+//            }
+//        }
     }
     
     func matchedTransitionSource_(id: some Hashable, in namespace: Namespace.ID) -> some View {
-        Group {
-            if #available(iOS 18.0, *) {
-                self.matchedTransitionSource(id: id, in: namespace, configuration: { config in
-                    config.clipShape(.rect(cornerRadius: 10))
-                })
-            } else {
-                self
-            }
-        }
+        // The code below requires Xcode 16, and is intentionally left commented for now
+        // until we upgrade to Xcode 16.
+        self
+//        Group {
+//            if #available(iOS 18.0, *) {
+//                self.matchedTransitionSource(id: id, in: namespace, configuration: { config in
+//                    config.clipShape(.rect(cornerRadius: 10))
+//                })
+//            } else {
+//                self
+//            }
+//        }
     }
 }
