142476160fa79fd2a3a05a2569e4c424d014566f
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-09-16 14:50:11 +0100

    Update View+NavigationTransition.swift

--- Mlem/App/Utility/Extensions/Views/View Modifiers/View+NavigationTransition.swift
+++ Mlem/App/Utility/Extensions/Views/View Modifiers/View+NavigationTransition.swift
@@ -9,26 +9,24 @@ import SwiftUI
 
 extension View {
     func navigationTransition_(sourceID: any Hashable, in namespace: Namespace.ID?) -> some View {
-        self
-//        Group {
-//            if #available(iOS 18.0, *), let namespace {
-//                self.navigationTransition(.zoom(sourceID: sourceID, in: namespace))
-//            } else {
-//                self
-//            }
-//        }
+        Group {
+            if #available(iOS 18.0, *), let namespace {
+                self.navigationTransition(.zoom(sourceID: sourceID, in: namespace))
+            } else {
+                self
+            }
+        }
     }
     
     func matchedTransitionSource_(id: some Hashable, in namespace: Namespace.ID) -> some View {
-        self
-//        Group {
-//            if #available(iOS 18.0, *) {
-//                self.matchedTransitionSource(id: id, in: namespace, configuration: { config in
-//                    config.clipShape(.rect(cornerRadius: 10))
-//                })
-//            } else {
-//                self
-//            }
-//        }
+        Group {
+            if #available(iOS 18.0, *) {
+                self.matchedTransitionSource(id: id, in: namespace, configuration: { config in
+                    config.clipShape(.rect(cornerRadius: 10))
+                })
+            } else {
+                self
+            }
+        }
     }
 }
