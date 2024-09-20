51ca3aeef442fa31249410c28868230fcc01df9e
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-09-20 15:17:00 -0400

    doesn't work

--- Mlem/App/Views/Shared/TestVideoView.swift
+++ Mlem/App/Views/Shared/TestVideoView.swift
@@ -14,47 +14,47 @@ import NukeUI
 import NukeVideo
 import SwiftUI
 
-/// A custom image view that supports downloading and displaying animated images.
-final class ImageView: UIView {
-    private let imageView: GIFImageView
-    private let spinner: UIActivityIndicatorView
-    private var task: ImageTask?
-
-    /* Initializers skipped */
-
-    func setImage(with url: URL) {
-        prepareForReuse()
-
-        if let response = ImagePipeline.shared.cache[url] {
-            imageView.display(response: response)
-            if !response.isPreview {
-                return
-            }
-        }
-
-        spinner.startAnimating()
-        task = ImagePipeline.shared.loadImage(with: url) { [weak self] result in
-            self?.spinner.stopAnimating()
-            if case let .success(response) = result {
-                self?.imageView.display(response: response)
-            }
-        }
-    }
-    
-    private func display(response: ImageResponse) {
-        if let data = response.container.data {
-            animate(withGIFData: data)
-        } else {
-            image = response.image
-        }
-    }
-    
-    private func prepareForReuse() {
-        task?.cancel()
-        spinner.stopAnimating()
-        imageView.prepareForReuse()
-    }
-}
+// A custom image view that supports downloading and displaying animated images.
+// final class ImageView: UIView {
+//    private let imageView: GIFImageView
+//    private let spinner: UIActivityIndicatorView
+//    private var task: ImageTask?
+//
+//    /* Initializers skipped */
+//
+//    func setImage(with url: URL) {
+//        prepareForReuse()
+//
+//        if let response = ImagePipeline.shared.cache[url] {
+//            imageView.display(response: response)
+//            if !response.isPreview {
+//                return
+//            }
+//        }
+//
+//        spinner.startAnimating()
+//        task = ImagePipeline.shared.loadImage(with: url) { [weak self] result in
+//            self?.spinner.stopAnimating()
+//            if case let .success(response) = result {
+//                self?.imageView.display(response: response)
+//            }
+//        }
+//    }
+//
+//    private func display(response: ImageResponse) {
+//        if let data = response.container.data {
+//            animate(withGIFData: data)
+//        } else {
+//            image = response.image
+//        }
+//    }
+//
+//    private func prepareForReuse() {
+//        task?.cancel()
+//        spinner.stopAnimating()
+//        imageView.prepareForReuse()
+//    }
+// }
 
 struct TestVideoView: UIViewRepresentable {
     let url: URL
@@ -62,27 +62,25 @@ struct TestVideoView: UIViewRepresentable {
     func makeUIView(context: Context) -> some UIView {
         let imageView = LazyImageView()
         imageView.makeImageView = { container in
-            if let type = container.type, type.isVideo, let asset = container.userInfo[.videoAssetKey] as? AVAsset {
-                if url.pathExtension.lowercased() == "gif" {
-//                    let view = FLAnimatedImageView()
-//                    Nuke.loadImage(with: URL(string: "http://.../cat.gif")!, into: view)
+            if let type = container.type {
+                if type == .gif, let data = container.data {
+                    print("It's a gif!")
                     let view = GIFImageView()
-                    // Nuke.loadImage(with: URL(string: "http://.../cat.gif")!, into: view)
-                    loadImage(
-                        with: .init(url: .init(string: "https://files.catbox.moe/6jr6cz.gif")!),
-                        into: view
-                    )
+                    view.animate(withGIFData: data)
+                    return view
+                } else {
+                    let view = UITextView()
+                    view.text = "Type is \(type.rawValue)"
                     return view
                 }
-                
-                let view = VideoPlayerView()
-                view.asset = asset
-                view.play()
+            } else {
+                let view = UITextView()
+                view.text = "No container"
                 return view
             }
-            return nil
         }
-        imageView.url = url
+        
+        // imageView.url = url
         return imageView
     }
 
