afddd6106ab2047ee9f3e087889af24a439ea173
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-11-01 20:55:17 +0000

    Purge Posts & Comments (#1420)

---
+++ Mlem/App/Views/Pages/ContentPurgeEditorView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentPurgeEditorView.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2024-10-26.
+//
+
+import MlemMiddleware
+import SwiftUI
+
+struct ContentPurgeEditorView: View {
+    @Environment(AppState.self) var appState
+    @Environment(Palette.self) var palette
+    @Environment(\.dismiss) var dismiss
+    
+    let target: any PurgableProviding
+    
+    @State var community: (any Community)?
+    @State var reason: String = ""
+    @FocusState var reasonFocused: Bool
+    @State var presentationSelection: PresentationDetent = .large
+    
+    init(target: any PurgableProviding) {
+        self.target = target
+        self._community = .init(wrappedValue: (target as? any Interactable2Providing)?.community)
+    }
+
+    var body: some View {
+        CollapsibleSheetView(presentationSelection: $presentationSelection, canDismiss: reason.isEmpty) {
+            NavigationStack {
+                Form {
+                    Section {
+                        WarningView(
+                            iconName: Icons.purge,
+                            text: "Purged content is erased from the database and cannot be restored.",
+                            inList: true
+                        )
+                    }
+                    Section {
+                        TextField("Reason (Optional)", text: $reason, axis: .vertical)
+                            .focused($reasonFocused)
+                    }
+                    ReasonPickerView(reason: $reason, community: community)
+                }
+                .scrollDismissesKeyboard(.interactively)
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationTitle("Purge")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Cancel") { dismiss() }
+                    }
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Send", systemImage: Icons.send) {
+                            Task {
+                                await send()
+                            }
+                        }
+                    }
+                }
+            }
+            .onAppear { reasonFocused = true }
+        }
+    }
+    
+    func send() async {
+        do {
+            try await target.purge(reason: reason.isEmpty ? nil : reason)
+            HapticManager.main.play(haptic: .success, priority: .low)
+            dismiss()
+        } catch {
+            handleError(error)
+        }
+    }
+}
