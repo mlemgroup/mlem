929083d478183216030cc9172de73dce84017ecc
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2024-12-13 18:32:47 +0000

    Date flairs in `PersonView` (#1497)

---
+++ Mlem/App/Utility/Extensions/Content Models/Profile2Providing+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Profile2Providing+Extensions.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2024-12-13.
+//
+
+import Foundation
+import MlemMiddleware
+
+extension Profile2Providing {
+    var isCakeDay: Bool { created.isAnniversaryToday }
+    
+    var createdRecently: Bool {
+        let intervalSinceCreation = Date.now.timeIntervalSince(created)
+        return intervalSinceCreation < 30 * 24 * 60 * 60
+    }
+}
