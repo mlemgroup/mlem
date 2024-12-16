d85e6f543f4fb40d39f09cf0d39eac7f4fae3a8a
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-12-16 18:55:16 -0500

    Fullscreen Image (#1499)

--- Mlem/App/Views/Pages/ImageViewer.swift
+++ Mlem/App/Views/Pages/ImageViewer.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct ImageViewer: View {
     @Environment(Palette.self) var palette
+    @Environment(MediaState.self) var mediaState
     
     let url: URL
+    
+    let duration: CGFloat = 0.25
+    let screenHeight: CGFloat = UIScreen.main.bounds.height
+    
+    @GestureState var dragState: Bool = false
+    
+    @State var isZoomed: Bool = false
+    @State var offset: CGFloat = 0
+    @State var isDismissing: Bool = false
+    @State var opacity: CGFloat = 0
     
     init(url: URL) {
         var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
@@ -19,15 +30,85 @@ struct ImageViewer: View {
     }
     
     var body: some View {
-        ZoomableContainer {
-            DynamicMediaView(url: url, playImmediately: true)
-                .padding(Constants.main.standardSpacing)
+        ZoomableContainer(isZoomed: $isZoomed) {
+            DynamicMediaView(url: url, cornerRadius: 0, playImmediately: true)
         }
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                CloseButtonView()
+        .offset(y: offset)
+        .background(.black)
+        .opacity(opacity)
+        .overlay(alignment: .topTrailing) {
+            if offset == 0 {
+                Button {
+                    fadeDismiss()
+                } label: {
+                    Image(systemName: Icons.close)
+                        .resizable()
+                        .frame(width: 15, height: 15)
+                        .foregroundStyle(.white)
+                        .padding([.top, .trailing], Constants.main.standardSpacing)
+                        .padding([.bottom, .leading], Constants.main.doubleSpacing)
+                        .contentShape(.rect)
+                }
+                .padding(Constants.main.standardSpacing)
             }
         }
-        .background(palette.background)
+        .simultaneousGesture(DragGesture(minimumDistance: 0.0)
+            .updating($dragState) { value, state, _ in
+                state = true
+                if !isZoomed, !isDismissing {
+                    offset = value.translation.height
+                    opacity = 1.0 - (abs(value.translation.height) / screenHeight)
+                }
+            }
+        )
+        .onAppear {
+            updateOpacity(1.0)
+        }
+        .onChange(of: dragState) {
+            if !dragState {
+                if abs(offset) > 100 {
+                    swipeDismiss(finalOffset: offset > 0 ? screenHeight : -screenHeight)
+                } else {
+                    updateDragDistance(0)
+                }
+            }
+        }
+    }
+    
+    private func fadeDismiss() {
+        isDismissing = true
+        updateOpacity(0) {
+            mediaState.url = nil
+        }
+    }
+    
+    private func swipeDismiss(finalOffset: CGFloat = UIScreen.main.bounds.height) {
+        isDismissing = true
+        updateDragDistance(finalOffset) {
+            mediaState.url = nil
+        }
+    }
+    
+    private func updateOpacity(_ newOpacity: CGFloat, callback: (() -> Void)? = nil) {
+        withAnimation(.easeOut(duration: duration)) {
+            opacity = newOpacity
+        }
+        if let callback {
+            DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                callback()
+            }
+        }
+    }
+    
+    private func updateDragDistance(_ newDistance: CGFloat, callback: (() -> Void)? = nil) {
+        withAnimation(.easeOut(duration: duration)) {
+            offset = newDistance
+            opacity = 1.0 - (abs(newDistance) / screenHeight)
+        }
+        if let callback {
+            DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
+                callback()
+            }
+        }
     }
 }
