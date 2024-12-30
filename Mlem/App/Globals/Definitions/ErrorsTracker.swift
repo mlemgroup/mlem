835a0c281e88c6e325489d68bf2bf0f6ac04507a
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2024-12-30 13:33:17 -0500

    luxury features

--- Mlem/App/Globals/Definitions/ErrorsTracker.swift
+++ Mlem/App/Globals/Definitions/ErrorsTracker.swift
@@ -13,8 +13,19 @@ class ErrorsTracker {
     
     @MainActor
     func addError(_ error: Error) {
-        errors.append(.init(error: error))
+        errors.prepend(.init(error: error))
     }
     
     static var main: ErrorsTracker = .init()
+    
+    func createErrorLog() -> String {
+        var ret: String = ""
+        
+        errors.forEach { details in
+            let description = details.error?.localizedDescription ?? "No Description"
+            ret += "\(details.when.formatted(.iso8601))\t\(details.title ?? "Error")\t\(description)\n"
+        }
+        
+        return ret
+    }
 }
--- Mlem/App/Views/Root/Tabs/Settings/ErrorLogView.swift
+++ Mlem/App/Views/Root/Tabs/Settings/ErrorLogView.swift
@@ -10,36 +10,74 @@ import SwiftUI
 struct ErrorLogView: View {
     @Environment(ErrorsTracker.self) var errorsTracker
     @Environment(Palette.self) var palette
+    @Environment(NavigationLayer.self) var navigation
     
     var body: some View {
         FancyScrollView {
             LazyVStack(spacing: Constants.main.standardSpacing) {
+                if errorsTracker.errors.isEmpty {
+                    Text("No errors")
+                        .foregroundStyle(palette.secondary)
+                }
                 ForEach(Array(errorsTracker.errors.enumerated()), id: \.offset) { _, errorDetails in
                     errorView(errorDetails)
                 }
+                .padding(.horizontal, Constants.main.standardSpacing)
             }
-            .padding(.horizontal, Constants.main.standardSpacing)
         }
         .background(palette.groupedBackground)
+        .navigationTitle("Error Log")
+        .toolbar {
+            if !errorsTracker.errors.isEmpty {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        Task {
+                            if let url = await downloadTextToFileSystem(
+                                fileName: "mlem_error_log.txt",
+                                text: errorsTracker.createErrorLog()
+                            ) {
+                                navigation.shareInfo = .init(url: url)
+                            } else {
+                                ToastModel.main.add(.failure("Failed to share error log"))
+                            }
+                        }
+                    } label: {
+                        Image(systemName: Icons.share)
+                    }
+                }
+            }
+        }
     }
     
     @ViewBuilder
     func errorView(_ details: ErrorDetails) -> some View {
         VStack(alignment: .leading, spacing: Constants.main.standardSpacing) {
-            // TODO: Copy
-            Text(details.title ?? "Error")
-                .fontWeight(.semibold)
+            HStack {
+                Text(details.title ?? "Error")
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                Button {
+                    if let text = details.error?.localizedDescription {
+                        UIPasteboard.general.string = text
+                        ToastModel.main.add(.success("Copied"))
+                    }
+                } label: {
+                    Text(Image(systemName: Icons.copy))
+                        .font(.subheadline)
+                        .foregroundStyle(palette.accent)
+                }
+            }
             
             Text(details.errorText)
             
-            // TODO: more human format
-            Text(details.when.formatted(.iso8601))
+            Text(details.when.formatted(date: .abbreviated, time: .standard))
                 .font(.caption)
                 .foregroundStyle(palette.secondary)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
         .padding(Constants.main.standardSpacing)
         .background(palette.secondaryGroupedBackground)
-        .clipShape(.rect(cornerRadius: Constants.main.smallItemCornerRadius))
+        .clipShape(.rect(cornerRadius: Constants.main.mediumItemCornerRadius))
     }
 }
