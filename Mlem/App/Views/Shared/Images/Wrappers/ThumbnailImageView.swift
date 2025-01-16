f341bc9da51adc57d8ca145b237bcf6f8b188887
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-01-16 16:41:04 -0500

    fix quicklook bug

--- Mlem/App/Views/Shared/Images/Wrappers/ThumbnailImageView.swift
+++ Mlem/App/Views/Shared/Images/Wrappers/ThumbnailImageView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import MlemMiddleware
 import SwiftUI
+import _QuickLook_SwiftUI // https://github.com/swiftlang/swift/issues/73048
 
 struct ThumbnailImageView: View {
     @Environment(Palette.self) var palette
@@ -70,7 +71,7 @@ struct ThumbnailImageView: View {
                             }
                         }
                     }
-                    // .quickLookPreview($quickLookUrl) // TODO: NOW wtf?
+                    .quickLookPreview($quickLookUrl)
             case let .link(link):
                 content
                     .onTapGesture {
