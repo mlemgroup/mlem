889d61bbf75d8a55bf9cc9d65fb8d0f6e645b19d
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-01-17 12:39:47 -0500

    fix material and play button not cooperating

--- Mlem/App/Views/Shared/Images/Helpers/AnimationControlLayer.swift
+++ Mlem/App/Views/Shared/Images/Helpers/AnimationControlLayer.swift
@@ -17,15 +17,32 @@ private struct AnimationControlLayer: ViewModifier {
     @Binding var animating: Bool
     var muted: Binding<Bool>?
     
+    // decouple play button state from blurred because the blur animation and material don't get along
+    @State var showPlayButton: Bool
+    
+    init(animating: Binding<Bool>, muted: Binding<Bool>?) {
+        self._animating = animating
+        self.muted = muted
+        self.showPlayButton = !animating.wrappedValue
+    }
+    
     func body(content: Content) -> some View {
         content
+            .onChange(of: blurred) {
+                if blurred {
+                    showPlayButton = false
+                } else {
+                    animating = true
+                    showPlayButton = true
+                }
+            }
             .overlay {
                 if animating {
                     Color.clear.contentShape(.rect)
                         .onTapGesture {
                             animating = false
                         }
-                } else if !blurred {
+                } else if showPlayButton {
                     PlayButton(postSize: .large)
                         .onTapGesture {
                             animating = true
