8b23121ea3804bed7b2b3c519e25ab665ab4192f
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-01-17 18:21:32 +0000

    Move extension from middleware

---
+++ Mlem/App/Utility/Extensions/Set+Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Set+Extensions.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2025-01-17.
+//
+
+import Foundation
+
+// https://stackoverflow.com/a/65598711/17629371
+extension Set: @retroactive RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let data = rawValue.data(using: .utf8),
+              let result = try? JSONDecoder().decode(Set<Element>.self, from: data)
+        else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        guard let data = try? JSONEncoder().encode(self),
+              let result = String(data: data, encoding: .utf8)
+        else {
+            return "[]"
+        }
+        return result
+    }
+}
