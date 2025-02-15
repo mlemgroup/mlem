35bb6df436a22bada01b786b1c520c7ef2a2b843
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-02-15 16:30:21 -0500

    Customizable Interaction Bar Widget Palette (#1744)
    Co-authored-by: Sjmarf <78750526+Sjmarf@users.noreply.github.com>

--- Mlem/App/Models/Action/CounterApperance+StaticValues.swift
+++ Mlem/App/Models/Action/CounterApperance+StaticValues.swift
@@ -11,19 +11,20 @@ extension CounterAppearance {
             value: value,
             leading: .upvote(isOn: upvoteOn),
             trailing: .downvote(isOn: downvoteOn),
-            label: "Score Counter"
+            label: "Score Counter",
+            singleIcon: Icons.scoreCounter
         )
     }
     
     static func upvote(value: Int = 9, isOn: Bool = false) -> CounterAppearance {
-        .init(value: value, leading: .upvote(isOn: isOn), trailing: nil, label: "Upvote Counter")
+        .init(value: value, leading: .upvote(isOn: isOn), trailing: nil, label: "Upvote Counter", singleIcon: Icons.upvoteCounter)
     }
     
     static func downvote(value: Int = 2, isOn: Bool = false) -> CounterAppearance {
-        .init(value: value, leading: .downvote(isOn: isOn), trailing: nil, label: "Downvote Counter")
+        .init(value: value, leading: .downvote(isOn: isOn), trailing: nil, label: "Downvote Counter", singleIcon: Icons.downvoteCounter)
     }
     
     static func reply(value: Int = 3) -> CounterAppearance {
-        .init(value: value, leading: .reply(), trailing: nil, label: "Reply Counter")
+        .init(value: value, leading: .reply(), trailing: nil, label: "Reply Counter", singleIcon: Icons.replyCounter)
     }
 }
