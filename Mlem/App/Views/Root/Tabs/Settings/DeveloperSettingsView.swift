97ad9bc97c0a86718d8067b3d505ff0490f448f5
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-02-20 22:40:07 -0500

    fixed DeveloperSettingsView

--- Mlem/App/Views/Root/Tabs/Settings/DeveloperSettingsView.swift
+++ Mlem/App/Views/Root/Tabs/Settings/DeveloperSettingsView.swift
@@ -14,89 +14,46 @@ import NukeUI
 // be burdening translators with these when they'll never be used
 
 struct DeveloperSettingsView: View {
-    // @Dependency(\.persistenceRepository) var persistenceRepository
+     @Dependency(\.persistenceRepository) var persistenceRepository
     
-    // @Setting(\.showFeedWelcomePrompt) var showFeedWelcomePrompt
-    // @Setting(\.developerMode) var developerMode
+     @Setting(\.showFeedWelcomePrompt) var showFeedWelcomePrompt
+     @Setting(\.developerMode) var developerMode
     
-    // @AppStorage("status.firstAppearance") var firstAppearance: Bool = true
-    
-    let url1: URL = .init(string: "https://sh.itjust.works/pictrs/image/df8dd5bb-7b80-4da8-8755-c4b35f9251aa.jpeg")!
-    let url2: URL = .init(string: "https://sh.itjust.works/pictrs/image/b1d77c25-6a3b-4202-9c30-6890be014d38.png")!
-    
-    @State var url: URL?
+     @AppStorage("status.firstAppearance") var firstAppearance: Bool = true
     
     var body: some View {
-        VStack {
-            FixedImageView(url: url, size: .init(width: 100, height: 100), fallback: .image, showProgress: true)
-            // MediaView(url: url)
+        Form {
+            Section {
+                Toggle(String("Developer Mode"), isOn: $developerMode)
+                NavigationLink(String("Error Log"), destination: .settings(.errorLog))
+            }
             
-            Button("change") {
-//                if url == url1 {
-//                    url = url2
-//                } else {
-//                    url = url1
-//                }
-                if url == nil {
-                    url = url1
-                } else if url == url1 {
-                    url = url2
-                } else {
-                    url = nil
+            #if DEBUG
+                Section {
+                    Button(String("Reset Feed Welcome Prompt")) {
+                        showFeedWelcomePrompt = true
+                    }
+                
+                    Button(String("Create Error")) {
+                        handleError(ApiClientError.insufficientPermissions)
+                    }
+                    
+                    Button(String("Create Silent Error")) {
+                        handleError(ApiClientError.noEntityFound, silent: true)
+                    }
+                } header: {
+                    Text(verbatim: "Debug Tools")
+                }
+            #endif
+            Button(String("Reset Settings State")) {
+                do {
+                    try persistenceRepository.deleteAllSystemSettings()
+                    firstAppearance = true
+                } catch {
+                    handleError(error)
                 }
             }
         }
-//        Form {
-//            Section {
-//                FixedImageView(url: url, size: .init(width: 100, height: 100), fallback: .image, showProgress: true)
-//                    .id(url)
-//                
-//                Button("change") {
-//                    if url == nil {
-//                        url = url1
-//                    } else if url == url1 {
-//                        url = url2
-//                    } else {
-//                        url = nil
-//                    }
-//                }
-//            }
-//            
-//            Section {
-//                Toggle(String("Developer Mode"), isOn: $developerMode)
-//                NavigationLink(String("Error Log"), destination: .settings(.errorLog))
-//            }
-//            
-//            #if DEBUG
-//                Section {
-//                    Button(String("Reset Feed Welcome Prompt")) {
-//                        showFeedWelcomePrompt = true
-//                    }
-//                
-//                    Button(String("Create Error")) {
-//                        handleError(ApiClientError.insufficientPermissions)
-//                    }
-//                    
-//                    Button(String("Create Silent Error")) {
-//                        handleError(ApiClientError.noEntityFound, silent: true)
-//                    }
-//                } header: {
-//                    Text(verbatim: "Debug Tools")
-//                }
-//            #endif
-//            Button(String("Reset Settings State")) {
-//                do {
-//                    try persistenceRepository.deleteAllSystemSettings()
-//                    firstAppearance = true
-//                } catch {
-//                    handleError(error)
-//                }
-//            }
-//        }
-//        .navigationTitle("Developer")
+        .navigationTitle("Developer")
     }
-}
-
-#Preview(traits: .sampleEnvironment) {
-    DeveloperSettingsView()
 }
