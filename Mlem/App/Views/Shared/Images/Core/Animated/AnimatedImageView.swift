65758b15d29e6577686f3460a2a0552093238831
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-03-19 18:19:47 -0400

    fix freeze, cleaning up

--- Mlem/App/Views/Shared/Images/Core/Animated/AnimatedImageView.swift
+++ Mlem/App/Views/Shared/Images/Core/Animated/AnimatedImageView.swift
@@ -14,48 +14,40 @@ struct AnimatedImageView: View {
     let data: Data
     
     @State var duration: CGFloat?
-    @State private var animationInfo: AnimationInfo = .init()
     
     var body: some View {
         UIAnimatedImageView(
             data: data,
-            animating: Binding(
-                get: { controlState.animating },
-                set: { _ in }
-            ),
             scrubTarget: Binding(
                 get: { controlState.scrubTarget },
                 set: { _ in }
-            ),
-            animationInfo: animationInfo
+            )
         )
-        .onChange(of: animationInfo.currentFrame) {
-            controlState.playbackPosition = animationInfo.progress
-        }
     }
 }
 
 private struct UIAnimatedImageView: UIViewRepresentable {
+    @Environment(MediaControlState.self) var controlState
+    
     let data: Data
     
-    @Binding var animating: Bool
     @Binding var scrubTarget: CGFloat?
-    var animationInfo: AnimationInfo
     
     @State var player: SDAnimatedImagePlayer?
     @State var observer: NSKeyValueObservation?
     
     func makeUIView(context: Context) -> SDAnimatedImageView {
         let imageView = SDAnimatedImageView()
-        imageView.autoPlayAnimatedImage = animating
+        imageView.autoPlayAnimatedImage = controlState.animating
         
         guard let animatedImage = SDAnimatedImage(data: data) else {
             handleError(MlemError.mediaError("Could not create animated image"))
             return imageView
         }
         
-        // preload all frames to improve scrubbing performance
+        // TODO: only if scrubbing enabled
         Task {
+            // loads all frames, which enables smooth backwards scrubbing
             animatedImage.preloadAllFrames()
         }
         
@@ -65,7 +57,7 @@ private struct UIAnimatedImageView: UIViewRepresentable {
             for index in (0..<animatedImage.animatedImageFrameCount) {
                 total += animatedImage.animatedImageDuration(at: index)
             }
-            animationInfo.duration = total
+            print("DEBUG runtime: \(total)")
         }
         
         // set up animation info with frame count and observation to update current frame
@@ -74,10 +66,9 @@ private struct UIAnimatedImageView: UIViewRepresentable {
                 assertionFailure("ImageView had nil player")
                 return
             }
-            animationInfo.totalFrames = Int(player.totalFrameCount)
-            observer = player.observe(\.currentFrameIndex) { _, _ in
-                print("DEBUG \(player.currentFrameIndex)")
-                // animationInfo.currentFrame = Int(player.currentFrameIndex)
+            print("DEBUG total frames: \(player.totalFrameCount)")
+            observer = player.observe(\.currentFrameIndex) { player, _ in
+                controlState.playbackPosition = CGFloat(player.currentFrameIndex) / CGFloat(player.totalFrameCount)
             }
             self.player = player
         }
@@ -98,40 +89,19 @@ private struct UIAnimatedImageView: UIViewRepresentable {
     
         if let scrubTarget {
             if player.isPlaying {
-                player.stopPlaying()
-            }
-            
-            // preload all frames of the image to improve scrubbing performance
-            if let animatedImage = uiView.image as? SDAnimatedImage,
-               !animatedImage.isAllFramesLoaded {
-                print("DEBUG loading all frames")
-                Task {
-                    animatedImage.preloadAllFrames()
-                }
+                player.pausePlaying()
             }
             
             player.seekToFrame(
                 at: .init((scrubTarget * CGFloat(player.totalFrameCount)).rounded()),
                 loopCount: 0
             )
-        } else if animating != player.isPlaying {
-            if animating {
+        } else if controlState.animating != player.isPlaying {
+            if controlState.animating {
                 player.startPlaying()
             } else {
-                player.stopPlaying()
+                player.pausePlaying()
             }
         }
-    }
-}
-
-@Observable
-private class AnimationInfo {
-    var duration: CGFloat?
-    var totalFrames: Int?
-    var currentFrame: Int?
-    
-    var progress: CGFloat {
-        guard let currentFrame, let totalFrames else { return 0 }
-        return CGFloat(currentFrame) / CGFloat(totalFrames)
     }
 }
