5aa763793602745c0a3254b08262da9eafb7754b
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-05-30 20:42:01 +0100

    Update

---
+++ Mlem/App/Views/Root/Login/Onboarding/OnboardingModel.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingModel.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2025-05-30.
+//
+
+import Foundation
+import MlemMiddleware
+
+@Observable
+class OnboardingModel {
+    enum Page { case recommendInstance, username, email }
+    
+    var page: Page = .recommendInstance
+    
+    var instance: Instance3?
+    var username: String?
+    var email: String?
+}
