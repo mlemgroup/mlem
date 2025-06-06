3aad57887508bffd49ceaa66105a8245a29ca623
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-06-06 21:48:57 +0100

    Update Comment2Snapshot.swift

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Snapshot/Comment/Comment2Snapshot.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Snapshot/Comment/Comment2Snapshot.swift
@@ -28,7 +28,7 @@ public struct Comment2Snapshot: CacheIdentifiable {
     
     public init(from comment: ApiCommentView) throws(ApiClientError) {
         self.comment = try .init(from: comment.comment)
-        self.creator = try .init(from: comment.commentCreator)
+        self.creator = try .init(from: comment.creator)
         self.post = try .init(from: comment.post)
         self.community = try .init(from: comment.community)
         
@@ -67,7 +67,7 @@ public struct Comment2Snapshot: CacheIdentifiable {
     
     public init(from report: ApiCommentReportView) throws(ApiClientError) {
         self.comment = try .init(from: report.comment)
-        self.creator = try .init(from: report.creator)
+        self.creator = try .init(from: report.commentCreator)
         self.post = try .init(from: report.post)
         self.community = try .init(from: report.community)
         
