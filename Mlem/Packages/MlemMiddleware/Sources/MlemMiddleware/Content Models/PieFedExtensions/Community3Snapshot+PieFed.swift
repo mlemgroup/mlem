28fce60b7418849f35788860779b1571fc6ef5f4
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-06-25 21:10:00 +0100

    Bug fix

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/PieFedExtensions/Community3Snapshot+PieFed.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/PieFedExtensions/Community3Snapshot+PieFed.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public extension Community3Snapshot {
     init(from community: PieFedGetCommunityResponse) throws(ApiClientError) {
-        self.community = try .init(from: community.communityView)
+        self.community = try .init(from: community.communityView, allPropertiesPresent: true)
         if let site = community.site {
             self.instance = try .init(from: site)
         } else {
