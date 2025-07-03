0f76ce79d588740fa5900a858a0da3cf0e467a4a
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-07-03 11:01:03 -0400

    added Mock methods to ApiRepository

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/API Client/ApiClient+Mock.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/API Client/ApiClient+Mock.swift
@@ -9,104 +9,27 @@ import Foundation
 import Rest
 
 #if DEBUG
-    public extension ApiClient {
-        static let mock: MockApiClient = .init()
-    }
 
-    public class MockApiClient: ApiClient {
-        public var posts: [Post2]
-        public var communities: [Community2]
-        public var people: [Person2]
-        public var comments: [Comment2]
-    
-        public init(
-            posts: [Post2] = [],
-            communities: [Community2] = [],
-            people: [Person2] = [],
-            comments: [Comment2] = []
-        ) {
-            self.posts = posts
-            self.communities = communities
-            self.people = people
-            self.comments = comments
-            let url = URL(string: "https://lemmy.world/")!
-            super.init(
-                url: url,
-                username: ""
-            )
-            self.token = "" // Not nil so that the views are interactable
-            let connection = LemmyConnection(baseUrl: url, token: "")
-            connection.setMockContext(.init(siteVersion: .v0_19_9, myPersonId: nil))
-            self.connection = connection
-        }
-    
-        override func perform<Request: RestRequest>(
-            _ request: Request,
-            tokenOverride: String? = nil,
-            requiresToken: Bool = true
-        ) async throws -> Request.Response {
-            if let request = request as? ListPostsRequest, let params = request.parameters {
-                return ApiGetPostsResponse(
-                    posts: posts.map(\.apiPostView),
-                    nextPage: nil,
-                    prevPage: nil
-                ) as! Request.Response
-            }
+public extension ApiClient {
+    static let mock: MockApiClient = .init()
+}
+
+public class MockApiClient: ApiClient {
+    public init(
+        posts: [Post2] = [],
+        communities: [Community2] = [],
+        people: [Person2] = [],
+        comments: [Comment2] = []
+    ) {
+        let url = URL(string: "https://lemmy.world/")!
+        let username = ""
+        super.init(
+            url: url,
+            username: username
+        )
         
-            if let request = request as? ListCommentsRequest, let params = request.parameters {
-                return ApiGetCommentsResponse(
-                    comments: comments.map(\.apiCommentView),
-                    nextPage: nil,
-                    prevPage: nil
-                ) as! Request.Response
-            }
-        
-            if let request = request as? ReadPersonRequest, let params = request.parameters {
-                if let person = people.first(where: { $0.id == params.personId })?.apiPersonView {
-                    return ApiGetPersonDetailsResponse(
-                        personView: person,
-                        comments: nil,
-                        posts: posts.filter { $0.creator.id == params.personId }.map(\.apiPostView),
-                        moderates: [],
-                        site: nil
-                    ) as! Request.Response
-                }
-            }
-        
-            if let request = request as? ResolveObjectRequest, let params = request.parameters {
-                return ApiResolveObjectResponse(
-                    comment: comments.first(where: { $0.actorId.description == params.q })?.apiCommentView,
-                    post: posts.first(where: { $0.actorId.description == params.q })?.apiPostView,
-                    community: communities.first(where: { $0.actorId.description == params.q })?.apiCommunityView,
-                    person: people.first(where: { $0.actorId.description == params.q })?.apiPersonView
-                ) as! Request.Response
-            }
-        
-            if let request = request as? GetCommunityRequest, let params = request.parameters {
-                if let community = communities.first(where: { $0.id == params.id })?.apiCommunityView {
-                    return ApiGetCommunityResponse(
-                        communityView: community,
-                        site: nil,
-                        moderators: [],
-                        discussionLanguages: []
-                    ) as! Request.Response
-                }
-            }
-        
-            if let request = request as? SearchRequest, let params = request.parameters {
-                return ApiSearchResponse(
-                    type_: params.type_,
-                    comments: [],
-                    posts: [],
-                    communities: params.type_ == .communities ? communities.map(\.apiCommunityView) : [],
-                    users: params.type_ == .users ? people.map(\.apiPersonView) : [],
-                    results: nil,
-                    nextPage: nil,
-                    prevPage: nil
-                ) as! Request.Response
-            }
-        
-            throw ApiClientError.insufficientPermissions
-        }
+        self.repository = MockApiRepository(url: url, username: username, posts: posts, communities: communities, people: people, comments: comments)
     }
+}
+
 #endif
