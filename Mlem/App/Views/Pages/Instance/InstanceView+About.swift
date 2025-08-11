42abe934d1cf7dda6a39dd65900522d20069f340
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-08-11 19:29:58 +0100

    UPdate

---
+++ Mlem/App/Views/Pages/Instance/InstanceView+About.swift
@@ -0,0 +1,32 @@
+//
+//  InstanceView+About.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2025-08-11.
+//
+
+import LemmyMarkdownUI
+import MlemMiddleware
+import SwiftUI
+
+extension InstanceView {
+    @ViewBuilder
+    func aboutTab(instance: any Instance) -> some View {
+        VStack(spacing: Constants.main.standardSpacing) {
+            if let shortDescription = instance.shortDescription {
+                markdownBox(shortDescription)
+            }
+            if let description = instance.description {
+                markdownBox(description)
+            }
+        }
+        .padding([.horizontal, .bottom], Constants.main.standardSpacing)
+    }
+    
+    private func markdownBox(_ text: String) -> some View {
+        Markdown(text, configuration: .default(palette: palette))
+            .padding(Constants.main.standardSpacing)
+            .background(.themedSecondaryGroupedBackground, in: .rect(cornerRadius: Constants.main.standardSpacing))
+            .paletteBorder(cornerRadius: Constants.main.standardSpacing)
+    }
+}
