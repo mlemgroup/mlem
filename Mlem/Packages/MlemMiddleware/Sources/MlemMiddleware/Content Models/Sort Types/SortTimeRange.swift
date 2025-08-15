56d6942187918dc92b786cbb88633b036918764f
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-08-15 21:21:50 +0100

    Bug fix

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Sort Types/SortTimeRange.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Sort Types/SortTimeRange.swift
@@ -30,7 +30,7 @@ public enum SortTimeRange: Hashable, Sendable {
     // This should only be used internally within ApiClient
     var timeRangeSeconds: Int {
         switch self {
-        case .allTime: Int32.max // Going higher than this value causes an error
+        case .allTime: Int(Int32.max) // Going higher than this value causes an error
         case let .limited(value): Int(value)
         }
     }
