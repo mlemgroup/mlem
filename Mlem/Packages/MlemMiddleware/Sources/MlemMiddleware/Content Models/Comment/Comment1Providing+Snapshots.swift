8ddd4dab761711f833a936e38ac574680c94c71f
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-08-15 15:57:38 -0400

    progress

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Comment/Comment1Providing+Snapshots.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Comment/Comment1Providing+Snapshots.swift
@@ -6,8 +6,14 @@
 //
 
 extension Comment1Providing {
-    public func snapshotUpdate(with snapshot: any CommentSnapshotProviding) {
-        
+    public func snapshotUpdate(with snapshot: any CommentSnapshotProviding) async {
+        if let comment2Snapshot = snapshot as? Comment2Snapshot {
+            snapshot1Update(with: comment2Snapshot.comment)
+        } else if let comment1Snapshot = snapshot as? Comment1Snapshot {
+            snapshotUpdate(with: comment1Snapshot)
+        } else {
+            assertionFailure("Unrecognized comment snapshot")
+        }
     }
     
     @MainActor
@@ -18,5 +24,26 @@ extension Comment1Providing {
         comment1.setIfChanged(\.languageId, snapshot.languageId)
         comment1.setIfChanged(\.deleted, snapshot.deleted)
         comment1.setIfChanged(\.removed, snapshot.removed)
+    }
+    
+    public func takeSnapshot() -> any CommentSnapshotProviding {
+        takeSnapshot1()
+    }
+    
+    public func takeSnapshot1() -> Comment1Snapshot {
+        return Comment1Snapshot(
+            actorId: actorId,
+            id: id,
+            creatorId: creatorId,
+            postId: postId,
+            parentCommentIds: parentCommentIds,
+            created: created,
+            content: content,
+            updated: updated,
+            distinguished: distinguished,
+            languageId: languageId,
+            deleted: deleted,
+            removed: removed
+        )
     }
 }
---
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Comment/Comment2Providing+Snapshots.swift
@@ -0,0 +1,17 @@
+//
+//  Comment2Providing+Snapshots.swift
+//  MlemMiddleware
+//
+//  Created by Eric Andrews on 2025-08-15.
+//
+
+extension Comment2Providing {
+    public func snapshotUpdate(with snapshot: any CommentSnapshotProviding) async {
+        
+    }
+    
+    @MainActor
+    internal func snapshot2Update(with snapshot: Comment2Snapshot) {
+        
+    }
+}
