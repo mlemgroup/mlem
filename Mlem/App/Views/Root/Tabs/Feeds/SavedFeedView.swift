a479a0390fe1554cd363fd7937ba6a1ba632b41a
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-08-15 21:35:53 +0100

    Implement requested changes

--- Mlem/App/Views/Root/Tabs/Feeds/SavedFeedView.swift
+++ Mlem/App/Views/Root/Tabs/Feeds/SavedFeedView.swift
@@ -16,15 +16,6 @@ struct SavedFeedView: View {
     @Environment(FiltersTracker.self) var filtersTracker
     @Environment(BackendClient.self) var backendClient
     
-    @Setting(\.post_size) var postSize
-    @Setting(\.feed_showRead) var showRead
-    @Setting(\.tip_feedWelcomePrompt) var showWelcomePrompt
-    @Setting(\.behavior_internetSpeed) var internetSpeed
-    @Setting(\.links_embedLoops) var embedLoops
-    
-    @AppStorage("lastTestFlightUpdate") var lastTestFlightUpdate: URL?
-
-    @ObservationIgnored @Dependency(\.persistenceRepository) private var persistenceRepository
     @State var savedFeedLoader: PersonContentFeedLoader?
     
     @State var scrollToTopTrigger: Bool = false
@@ -60,79 +51,9 @@ struct SavedFeedView: View {
     @ViewBuilder
     var content: some View {
         FancyScrollView(scrollToTopTrigger: $scrollToTopTrigger) {
-            if AccountsTracker.main.isEmpty, showWelcomePrompt, !appState.firstApi.willSendToken {
-                FeedWelcomeView()
-                    .padding([.horizontal, .bottom], Constants.main.standardSpacing)
-            }
-            if Bundle.main.isTestFlight, let testflightUrl = backendClient.testflightUpdate, lastTestFlightUpdate != testflightUrl {
-                UpdateBannerView(url: testflightUrl)
-                    .padding([.horizontal, .bottom], Constants.main.standardSpacing)
-            }
             if let savedFeedLoader {
                 PersonContentGridView(feedLoader: savedFeedLoader, contentType: .constant(.all))
             }
         }
-        .animation(.snappy, value: backendClient.testflightUpdate != lastTestFlightUpdate)
     }
 }
-
-private struct FeedSelectionTitleModifier: ViewModifier {
-    let feedOptions: [FeedSelection]
-    let shouldScrollToTop: Bool
-    @Binding var feedSelection: FeedSelection
-    @Binding var scrollToTopTrigger: Bool
-    
-    @State var isAtTop: Bool = false
-    
-    func body(content: Content) -> some View {
-        content
-            .toolbar {
-                if !isAtTop {
-                    ToolbarTitleMenu {
-                        FeedSelectionMenuView(
-                            feedOptions: feedOptions,
-                            shouldScrollToTop: shouldScrollToTop,
-                            feedSelection: $feedSelection,
-                            scrollToTopTrigger: $scrollToTopTrigger
-                        )
-                    }
-                }
-            }
-            .isAtTopSubscriber(isAtTop: $isAtTop)
-    }
-}
-
-private struct FeedSelectionMenuView: View {
-    let feedOptions: [FeedSelection]
-    let shouldScrollToTop: Bool
-    @Binding var feedSelection: FeedSelection
-    @Binding var scrollToTopTrigger: Bool
-    
-    var body: some View {
-        ForEach(feedOptions, id: \.self) { feed in
-            Button(
-                String(localized: feed.description.label),
-                icon: feed.description.icon
-            ) {
-                if shouldScrollToTop {
-                    scrollToTopTrigger.toggle()
-                    // delay feed switch to allow scroll to complete
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        feedSelection = feed
-                    }
-                } else {
-                    feedSelection = feed
-                }
-            }
-            .symbolVariant(feedSelection == feed ? .fill : .none)
-        }
-    }
-}
-
-#if DEBUG
-    #Preview(traits: .sampleEnvironment(api: .realistic)) {
-        FeedsView()
-            .previewNavigationStack(backButtonLabel: "Feeds")
-            .previewTabBar(selected: .feeds)
-    }
-#endif
