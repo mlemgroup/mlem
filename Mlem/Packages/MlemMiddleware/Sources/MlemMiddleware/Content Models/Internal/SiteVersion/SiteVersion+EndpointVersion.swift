909c749970847ee20924bff750afa8ce32858c3a
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-09-22 22:21:10 +0100

    Rename `SiteVersion.EndpointVersion` -> `LemmyEndpointVersion` (#2310)

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Internal/SiteVersion/SiteVersion+EndpointVersion.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Internal/SiteVersion/SiteVersion+EndpointVersion.swift
@@ -7,15 +7,13 @@
 
 import Foundation
 
-public extension SiteVersion {
-    enum EndpointVersion: Hashable, Sendable {
-        case v3, v4
-        
-        public var pathComponent: String {
-            switch self {
-            case .v3: "v3"
-            case .v4: "v4"
-            }
+enum LemmyEndpointVersion: Hashable, Sendable {
+    case v3, v4
+    
+    var pathComponent: String {
+        switch self {
+        case .v3: "v3"
+        case .v4: "v4"
         }
     }
 }
