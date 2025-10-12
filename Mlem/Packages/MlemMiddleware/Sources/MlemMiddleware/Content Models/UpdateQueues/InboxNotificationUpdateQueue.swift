43b60c62ad9671fac652fba3653f88eecff3fb6e
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-10-12 20:08:37 +0100

    Update InboxNotificationQueue documentation

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/UpdateQueues/InboxNotificationUpdateQueue.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/UpdateQueues/InboxNotificationUpdateQueue.swift
@@ -1,5 +1,5 @@
 //
-//  PostUpdateQueue.swift
+//  InboxNotificationUpdateQueue.swift
 //  MlemMiddleware
 //
 //  Created by Eric Andrews on 2025-07-04.
@@ -7,19 +7,15 @@
 
 import Semaphore
 
-/// This actor synchronizes state updates for a particular post.
+/// This actor synchronizes state updates for a particular inbox notification.
 ///
-/// Calls are queued using `addItem`, and each call must return a `PostSnapshotProviding`. When each call returns, `lastVerifiedSnapshot` is updated
+/// Calls are queued using `addItem`, and each call must return an `InboxNotificationSnapshot`. When each call returns, `lastVerifiedSnapshot` is updated
 /// with the returned snapshot. Each call will only execute when the previous one finishes, ensuring that `lastVerifiedSnapshot` always accurately reflects
 /// the most recently queried server state.
 ///
 /// When the queue finishes executing altogether, it updates its parent model with the most recent snapshot. State faking is performed by the model
 /// before the work item is queued.
 ///
-/// - Note: some care must be taken to ensure that `parent` always points to a valid model. When a model is initialized, it updates `parent` to be itself;
-/// likewise, when a model is deinitialized, it updates `parent` to be the next lower-tier model contained within itself (e.g., `Post3`'s deinit updates parent
-/// to be `post2`). If this update is not performed, `parent` may become nil and the queue will refuse to execute. In debug mode this will throw an error,
-/// while in production the queue will simply not run until an item is added when the parent is present.
 public actor InboxNotificationUpdateQueue {
     weak var parent: InboxNotification?
     
@@ -63,8 +59,6 @@ public actor InboxNotificationUpdateQueue {
         }
         print("DEBUG executing queue")
         
-        // assigning this here ensures parent stays in scope for the duration of the queue. For operations that remove the post
-        // (e.g., hide), if the call is slow, the parent might go out of scope before it returns; this in turn breaks the undo behavior
         guard let parent else {
             assertionFailure("Cannot execute queue with no parent!")
             return
