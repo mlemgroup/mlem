b94a62ed8ecb198a947f13a0c78498400c3fe78e
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-10-17 14:26:00 +0100

    fixup! Add edit action

---
+++ Mlem/App/Actions/EditAction.swift
@@ -0,0 +1,66 @@
+//
+//  EditAction.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2025-10-17.
+//
+
+import Actions
+import MlemMiddleware
+import SwiftUI
+
+struct EditAction: ConfigurableAction {
+    enum Content {
+        case message(any Message1Providing)
+        
+        var value: any OwnershipProviding {
+            switch self {
+            case let .message(message): message
+            }
+        }
+    }
+    
+    let content: Content
+}
+
+// MARK: - Configurability
+
+extension ActionSeed {
+    static let edit = ActionSeed("edit") { entity in
+        switch entity {
+        case let entity as any Message1Providing: EditAction(content: .message(entity))
+        default: nil
+        }
+    }
+}
+
+// MARK: - Appearance
+
+extension EditAction {
+    static let label: ActionLabel = .init("Edit", icon: .general.edit)
+    
+    func createLabel(environment: EnvironmentValues) -> ActionLabel {
+        Self.label.withVisibility(visibility)
+    }
+    
+    private var visibility: ActionVisiblity {
+        guard let myPersonId = content.value.api.myPerson?.id else { return .hidden }
+        return content.value.isOwnContent(myPersonId: myPersonId) ? .enabled : .hidden
+    }
+}
+
+// MARK: - Behavior
+
+extension EditAction {
+    @MainActor
+    func execute(environment: EnvironmentValues) {
+        switch content {
+        case let .message(message):
+            if let message = message as? any Message2Providing {
+                environment.editMessage(message.message2)
+            } else {
+                assertionFailure()
+            }
+        }
+    }
+}
