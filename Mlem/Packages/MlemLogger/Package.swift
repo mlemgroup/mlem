d341a1d2fe1dcc416021c968890f09bc9164de9e
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-10-26 16:45:27 -0400

    revert platform version

--- Mlem/Packages/MlemLogger/Package.swift
+++ Mlem/Packages/MlemLogger/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MlemLogger",
-    platforms: [.iOS(.v18)],
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
