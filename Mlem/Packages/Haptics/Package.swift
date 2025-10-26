073568c429e3640f1b7846cc2c65da2091b483cb
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-10-26 17:27:55 -0400

    everything but Theming

--- Mlem/Packages/Haptics/Package.swift
+++ Mlem/Packages/Haptics/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Haptics",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -26,7 +26,10 @@ let package = Package(
                 .byName(name: "MlemLogger")
             ],
             resources: [.process("Resources")],
-            swiftSettings: [.enableUpcomingFeature("BareSlashRegexLiterals")]
+            swiftSettings: [
+                .swiftLanguageMode(.v5),
+                .enableUpcomingFeature("BareSlashRegexLiterals")
+            ]
         )
     ]
 )
