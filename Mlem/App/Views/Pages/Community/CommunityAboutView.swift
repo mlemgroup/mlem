24ec6179060df70f3661590282318d74382378a4
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-10-30 19:48:43 +0000

    Extract description view to computed var

--- Mlem/App/Views/Pages/Community/CommunityAboutView.swift
+++ Mlem/App/Views/Pages/Community/CommunityAboutView.swift
@@ -22,24 +22,29 @@ struct CommunityAboutView: View {
                 MediaView.largeImage(url: banner, shouldBlur: false)
             }
             if let description = community.description {
-                VStack(alignment: .trailing) {
-                    if canEditDescription {
-                        Button("Edit", icon: .general.edit) {
-
-                        }
-                        .font(.title)
-                        .labelStyle(.iconOnly)
-                        .symbolVariant(.circle.fill)
-                        .foregroundStyle(.themedPrimary, .themedTertiaryGroupedBackground)
-                    }
-                    Markdown(description, configuration: .default(palette: palette))
-                }
-                .padding(Constants.main.standardSpacing)
-                .background(.themedSecondaryGroupedBackground, in: .rect(cornerRadius: Constants.main.standardSpacing))
-                .paletteBorder(cornerRadius: Constants.main.standardSpacing)
+                descriptionView(description)
             }
         }
         .padding([.horizontal, .bottom], Constants.main.standardSpacing)
+    }
+
+    @ViewBuilder
+    func descriptionView(_ description: String) -> some View {
+        VStack(alignment: .trailing) {
+            if canEditDescription {
+                Button("Edit", icon: .general.edit) {
+
+                }
+                .font(.title)
+                .labelStyle(.iconOnly)
+                .symbolVariant(.circle.fill)
+                .foregroundStyle(.themedPrimary, .themedTertiaryGroupedBackground)
+            }
+            Markdown(description, configuration: .default(palette: palette))
+        }
+        .padding(Constants.main.standardSpacing)
+        .background(.themedSecondaryGroupedBackground, in: .rect(cornerRadius: Constants.main.standardSpacing))
+        .paletteBorder(cornerRadius: Constants.main.standardSpacing)
     }
 
     var canEditDescription: Bool {
