b2c8aed04891b90f87d3adc85a7a1e8b2b05e9f0
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-11-07 12:39:50 +0000

    MarkReadAction

---
+++ Mlem/App/Actions/MarkReadAction.swift
@@ -0,0 +1,57 @@
+//
+//  MarkReadAction.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2025-11-07.
+//  
+
+import Actions
+import MlemMiddleware
+import SwiftUI
+
+struct MarkReadAction: ConfigurableAction {
+    let notification: InboxNotification
+}
+
+// MARK: - Configurability
+
+extension ActionSeed {
+    static let markRead = ActionSeed("markRead") { entity in
+        switch entity {
+        case let entity as InboxNotification: MarkReadAction(notification: entity)
+        default: nil
+        }
+    }
+}
+
+// MARK: - Appearance
+
+extension MarkReadAction {
+    static let markReadLabel: ActionLabel = .init("Mark Read", icon: .lemmy.markRead)
+    static let markUnreadLabel: ActionLabel = .init("Mark Unread", icon: .lemmy.markUnread)
+    
+    static var label: ActionLabel { markReadLabel }
+
+    func createLabel(environment: EnvironmentValues) -> ActionLabel {
+        if notification.read {
+            Self.markUnreadLabel.withVisibility(visibility(environment))
+        } else {
+            Self.markReadLabel.withVisibility(visibility(environment))
+        }
+    }
+
+    private func visibility(_ environment: EnvironmentValues) -> ActionVisiblity {
+        guard notification.api.canInteract(appState: environment.appState) else { return .hidden }
+        return .enabled
+    }
+}
+
+// MARK: - Behavior
+
+extension MarkReadAction {
+    @MainActor
+    func execute(environment: EnvironmentValues) {
+        notification.toggleRead()
+        environment.hapticManager.play(haptic: .lightSuccess, tier: .low)
+    }
+}
