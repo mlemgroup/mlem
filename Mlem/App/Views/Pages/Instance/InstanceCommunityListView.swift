732d19b39265a6b05d679112b42c0ea4ba2033c5
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-11-09 10:32:29 +0000

    Community list error handling

--- Mlem/App/Views/Pages/Instance/InstanceCommunityListView.swift
+++ Mlem/App/Views/Pages/Instance/InstanceCommunityListView.swift
@@ -11,23 +11,30 @@ import SwiftUI
 struct InstanceCommunityListView: View {
     let communityLoader: CommunityFeedLoader
 
+    @State var errorDetails: ErrorDetails?
+
     var body: some View {
         LazyVStack(spacing: 0) {
-            SearchResultsView(results: communityLoader.items) { community in
-                CommunityListRow(
-                    community,
-                    readout: .subscribers,
-                    visitContext: .other
-                )
-                .onAppear {
-                    do {
-                        try communityLoader.loadIfThreshold(community)
-                    } catch {
-                        handleError(error)
+            if let errorDetails {
+                ErrorView(errorDetails)
+                    .padding(.top, 30)
+            } else {
+                SearchResultsView(results: communityLoader.items) { community in
+                    CommunityListRow(
+                        community,
+                        readout: .subscribers,
+                        visitContext: .other
+                    )
+                    .onAppear {
+                        do {
+                            try communityLoader.loadIfThreshold(community)
+                        } catch {
+                            handleError(error)
+                        }
                     }
                 }
+                EndOfFeedView(feedLoader: communityLoader, viewType: .hobbit)
             }
-            EndOfFeedView(feedLoader: communityLoader, viewType: .hobbit)
         }
         .animation(.easeOut(duration: 0.1), value: communityLoader.items.isEmpty)
         .task { await refresh() }
@@ -38,8 +45,25 @@ struct InstanceCommunityListView: View {
             if communityLoader.loadingState == .initial {
                 try await communityLoader.refresh(listing: .local)
             }
+            self.errorDetails = nil
         } catch {
-            handleError(error)
+            var errorDetails = handleErrorWithDetails(error)
+
+            errorDetails?.refresh = {
+                await refresh()
+                return true
+            }
+
+            if case let ApiClientError.response(response, _) = error {
+                if response.instanceIsPrivate {
+                    errorDetails?.title = "Private Instance"
+                    errorDetails?.body = "You cannot view the communities of a private instance."
+                    errorDetails?.icon = .lemmy.private
+                    errorDetails?.refresh = nil
+                }
+            }
+
+            self.errorDetails = errorDetails
         }
     }
 }
