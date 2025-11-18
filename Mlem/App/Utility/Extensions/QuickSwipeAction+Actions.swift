1716f2ad24642977b5ea763c8787b8e60f798308
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-11-18 14:34:23 +0000

    Update

---
+++ Mlem/App/Utility/Extensions/QuickSwipeAction+Actions.swift
@@ -0,0 +1,60 @@
+//
+//  QuickSwipeAction+Actions.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2025-11-18.
+//
+
+import Actions
+import Icons
+import QuickSwipes
+import SwiftUI
+
+private extension QuickSwipeAction {
+    init?(label: ActionLabel, callback: @escaping () -> Void) {
+        if label.visibility == .hidden { return nil }
+        self.init(
+            icon: label.icon,
+            color: .themedAccent,
+            enabled: label.visibility == .enabled,
+            confirmationPrompt: nil,
+            callback: callback
+        )
+    }
+}
+
+private struct QuickSwipesActionsViewModifier: ViewModifier {
+    @Environment(\.self) var environment
+
+    let leadingActions: [any Actions.Action]
+    let trailingActions: [any Actions.Action]
+
+    func body(content: Content) -> some View {
+        content
+            .quickSwipes(config)
+    }
+
+    var config: SwipeConfiguration {
+        .init(
+            leadingActions: leadingActions.compactMap(self.createAction),
+            trailingActions: trailingActions.compactMap(self.createAction)
+        )
+    }
+
+    func createAction(_ action: any Actions.Action) -> QuickSwipeAction? {
+        .init(
+            label: action.createLabel(environment: environment),
+            callback: { action.execute(environment: environment) }
+        )
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func quickSwipes(leading: [any Actions.Action], trailing: [any Actions.Action]) -> some View {
+        modifier(QuickSwipesActionsViewModifier(
+            leadingActions: leading,
+            trailingActions: trailing
+        ))
+    }
+}
--- Mlem/App/Views/Shared/TestInboxView.swift
+++ Mlem/App/Views/Shared/TestInboxView.swift
@@ -5,8 +5,10 @@
 //  Created by Sjmarf on 2025-10-01.
 //
 
+import Actions
 import MlemMiddleware
 import SwiftUI
+import QuickSwipes
 
 // This view is for testing only and will be removed once the notification system is working
 struct TestInboxView: View {
@@ -38,42 +40,45 @@ private struct TestInboxSectionView: View {
         VStack(alignment: .leading, spacing: 10) {
             Text(type.rawValue)
             if let notifications {
-                ForEach(notifications) { notification in
-                    VStack {
-                        if notification.read {
-                            Image(icon: .general.success)
-                                .foregroundStyle(.blue)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                        }
-                        itemBody(notification.content)
-                            .lineLimit(2)
-                    }
-                    .lineLimit(3)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(10)
-                    .background(.themedSecondaryGroupedBackground, in: .rect(cornerRadius: 10))
-                    .contextMenu(notification: notification)
-                    .popupAnchor()
-                    .onTapGesture {
-                        Task {
-                            notification.toggleRead()
-                        }
-                    }
-                }
+                ForEach(notifications, content: self.item)
             }
         }
         .task { await load() }
     }
 
     @ViewBuilder
-    func itemBody(_ content: InboxNotificationContent) -> some View {
-        switch content {
-        case let .reply(comment):
-            Text(comment.content)
-        case let .mention(comment):
-            Text(comment.content)
-        case let .message(message):
-            Text(message.content)
+    func item(_ notification: InboxNotification) -> some View {
+            itemBody(notification)
+                .lineLimit(2)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(10)
+            .background(.themedSecondaryGroupedBackground, in: .rect(cornerRadius: 10))
+            .contextMenu(notification: notification)
+            .quickSwipes(leading: [], trailing: [MarkReadAction(notification: notification)])
+            .popupAnchor()
+            .onTapGesture {
+                Task {
+                    notification.toggleRead()
+                }
+            }
+    }
+
+    @ViewBuilder
+    func itemBody(_ notification: InboxNotification) -> some View {
+        VStack {
+            if notification.read {
+                Image(icon: .general.success)
+                    .foregroundStyle(.blue)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+            switch notification.content {
+            case let .reply(comment):
+                Text(comment.content)
+            case let .mention(comment):
+                Text(comment.content)
+            case let .message(message):
+                Text(message.content)
+            }
         }
     }
     
