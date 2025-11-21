ab8aad56642dbd416dc7683121a232aa51bba0a1
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-11-21 16:28:34 -0500

    clean up

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/FeedLoaders/Filtering/Filters/Post/PostKeywordFilter.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/FeedLoaders/Filtering/Filters/Post/PostKeywordFilter.swift
@@ -8,37 +8,10 @@
 import Foundation
 
 class PostKeywordFilter: FilterProviding<Post2> {
-//    typealias FilterTarget = Post2
-//    
-//    var numFiltered: Int = 0
-//    private var context: FilterContext
-//    var active: Bool = true
-//    
-//    init(context: FilterContext) {
-//        self.context = context
-//    }
-//    
-//    func filter(_ targets: [Post2]) -> [Post2] {
-//        let ret = targets.filter(shouldPassFilter)
-//        numFiltered += targets.count - ret.count
-//        return ret
-//    }
-//    
-//    func reset(with targets: [Post2]?) -> [Post2] {
-//        numFiltered = 0
-//        if let targets { return filter(targets) }
-//        return .init()
-//    }
-//    
-    /// Returns true if the given post should pass the filter, false otherwise
     override public func shouldPassFilter(_ post: Post2) -> Bool {
         // bypass filter for moderated/administrated posts
         if context.isAdmin || context.moderatedCommunityActorIds.contains(post.community.actorId) { return true }
         
         return !post.title.failsKeywordFilter(keywords: context.filteredKeywords, phrases: context.filteredPhrases)
     }
-//    
-//    func updateFilterContext(to context: FilterContext) {
-//        self.context = context
-//    }
 }
