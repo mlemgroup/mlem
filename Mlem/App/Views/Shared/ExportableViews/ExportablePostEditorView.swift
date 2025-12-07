b77ad0643f6f5eb24f0fc4d8d9b0df64222b3710
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-12-07 11:15:00 -0500

    Save comment as image (#2479)

--- Mlem/App/Views/Shared/ExportableViews/ExportablePostEditorView.swift
+++ Mlem/App/Views/Shared/ExportableViews/ExportablePostEditorView.swift
@@ -17,14 +17,15 @@ struct ExportablePostEditorView: View {
     @Environment(NavigationLayer.self) var navigation
     @Environment(AppState.self) var appState
     @Environment(HapticManager.self) var hapticManager
+    
     @Environment(\.colorScheme) var colorScheme
     @Setting(\.appearance_palette) var palette
+    @Setting(\.post_createImage_showCommunity) var showCommunity
+    @Setting(\.post_createImage_showCreator) var showCreator
+    @Setting(\.post_createImage_showStats) var showStats
+    @Setting(\.post_createImage_colorScheme) var overrideColorScheme
     
     let post: any Post1Providing
-    @State var showCommunity: Bool = true
-    @State var showCreator: Bool = true
-    @State var showStats: Bool = true
-    @State var overrideColorScheme: UIUserInterfaceStyle = .unspecified
     
     var overriddenColorScheme: ColorScheme {
         switch overrideColorScheme {
@@ -34,46 +35,22 @@ struct ExportablePostEditorView: View {
         default: .light
         }
     }
-    
-    @State var snapshot: UIImage?
-    
-    var snapshotRenderHashValue: Int {
-        var hasher = Hasher()
-        hasher.combine(showCommunity)
-        hasher.combine(showCreator)
-        hasher.combine(showStats)
-        hasher.combine(overriddenColorScheme)
-        return hasher.finalize()
-    }
 
     var body: some View {
         ScrollView {
             exportablePost
                 .padding(.bottom, 200)
         }
-        .task(id: snapshotRenderHashValue) {
-            snapshot = createImageFromView(exportablePost)
-        }
         .presentationBackground(.themedGroupedBackground)
         .overlay(alignment: .bottom) {
-            Group {
-                if #available(iOS 26, *) {
-                    controls
-                        .glassEffect(.regular.interactive(), in: .capsule)
-                } else {
-                    controls
-                        .background(.regularMaterial, in: .capsule)
-                }
-            }
-            .padding(.horizontal, 50)
-            .padding(Constants.main.standardSpacing)
+            ExportableViewControlOverlay { createImageFromView(exportablePost) }
         }
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 CloseButtonView(ios18Label: .cancel)
             }
             ToolbarItem(placement: .topBarTrailing) {
-                Menu("Details", systemImage: "slider.horizontal.3") {
+                Menu("Details", icon: .general.configure) {
                     Toggle("Community", icon: .lemmy.community, isOn: $showCommunity)
                     Toggle("Creator", icon: .lemmy.person, isOn: $showCreator)
                     Toggle("Stats", icon: .lemmy.votes, isOn: $showStats)
@@ -92,68 +69,13 @@ struct ExportablePostEditorView: View {
             }
         }
     }
-    
-    @ViewBuilder
-    var controls: some View {
-        HStack {
-            saveButton
-            shareButton
-        }
-        .font(.title2)
-        .labelStyle(.iconOnly)
-        .buttonStyle(.plain)
-        .padding(.horizontal, Constants.main.halfSpacing)
-    }
-    
-    @ViewBuilder var saveButton: some View {
-        if let imageData = snapshot?.pngData() {
-            Button("Save", icon: .general.import) {
-                Task {
-                    do {
-                        try await ImageSaver().writeImageToPhotoAlbum(imageData: imageData)
-                        ToastModel.main.add(.success("Image Saved"))
-                    } catch {
-                        handleError(error)
-                    }
-                }
-            }
-            .padding(Constants.main.standardSpacing)
-            .contentShape(.rect)
-        } else {
-            ProgressView()
-        }
-    }
-    
-    @ViewBuilder
-    var shareButton: some View {
-        if let imageData = snapshot?.pngData(),
-           let fileUrl = createTempFile(data: imageData, fileName: "post.png") {
-            ShareLink(item: fileUrl)
-                .padding(Constants.main.standardSpacing)
-                .contentShape(.rect)
-        } else {
-            ProgressView()
-        }
-    }
         
     var exportablePost: some View {
         ExportablePostView(
             post: post,
             appState: appState,
-            colorScheme: overriddenColorScheme,
-            showCommunity: showCommunity,
-            showCreator: showCreator,
-            showStats: showStats
+            colorScheme: overriddenColorScheme
         )
         .allowsHitTesting(false)
-    }
-    
-    private func createTempFile(data: Data, fileName: String) -> URL? {
-        do {
-            return try data.writeToTempFile(fileName: fileName)
-        } catch {
-            handleError(error)
-            return nil
-        }
     }
 }
