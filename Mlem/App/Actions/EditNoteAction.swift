fb05ef69c3f8937591c9196dd217bc722e04e485
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2025-12-19 10:50:44 +0000

    Show "add note" if no note exists for user

--- Mlem/App/Actions/EditNoteAction.swift
+++ Mlem/App/Actions/EditNoteAction.swift
@@ -9,14 +9,17 @@ import Actions
 import MlemMiddleware
 import SwiftUI
 
-struct EditNoteAction: SimpleLabelAction {
+struct EditNoteAction: Actions.Action {
     let entity: any Person1Providing
 }
 
 // MARK: - Configurability
 
 extension ActionSeed {
-    static let editNote = ActionSeed("editNote") { entity in
+    static let editNote = ActionSeed(
+        "editNote",
+        label: EditNoteAction.createLabel(noteExists: true)
+    ) { entity in
         switch entity {
         case let entity as any Person1Providing: EditNoteAction(entity: entity)
         default: nil
@@ -27,10 +30,17 @@ extension ActionSeed {
 // MARK: - Appearance
 
 extension EditNoteAction {
-    static let label: ActionLabel = .init("Edit Note", icon: .lemmy.editNote)
+    static func createLabel(noteExists: Bool) -> ActionLabel {
+        if noteExists {
+            .init("Edit Note", icon: .lemmy.editNote)
+        } else {
+            .init("Add Note", icon: .lemmy.editNote)
+        }
+    }
 
     func createLabel(environment: EnvironmentValues) -> ActionLabel {
-        Self.label.withVisibility(entity.api.supports(.userNotes, defaultValue: false) ? .enabled : .hidden)
+        Self.createLabel(noteExists: entity.note != nil)
+            .withVisibility(entity.api.supports(.userNotes, defaultValue: false) ? .enabled : .hidden)
     }
 }
 
