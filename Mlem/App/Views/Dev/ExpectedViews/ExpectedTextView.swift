d510395785b4af97f8e374a309202a1835dfa87f
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2025-12-27 19:57:47 -0500

    use .redacted for ExpectedTextView

--- Mlem/App/Views/Dev/ExpectedViews/ExpectedTextView.swift
+++ Mlem/App/Views/Dev/ExpectedViews/ExpectedTextView.swift
@@ -9,34 +9,13 @@ import SwiftUI
 import ComponentViews
 import MlemMiddleware
 
-extension Font {
-    // source: https://sarunw.com/posts/scaling-custom-fonts-automatically-with-dynamic-type/
-    var leadingHeight: CGFloat {
-        switch self {
-        case .largeTitle: 41
-        case .title: 34
-        case .title2: 28
-        case .title3: 25
-        case .headline: 22
-        case .body: 22
-        case .callout: 21
-        case .subheadline: 20
-        case .footnote: 18
-        case .caption: 16
-        case .caption2: 13
-        default:  22
-        }
-    }
-}
-
 struct ExpectedText: View {
-    @Environment(\.font) var _font: Font?
-    var font: Font { _font ?? .body }
-    
     let text: ExpectedValue<String>
-    
-    init(_ text: ExpectedValue<String>) {
+    private let placeholder: String
+
+    init(_ text: ExpectedValue<String>, expectedLength: Int = 15) {
         self.text = text
+        self.placeholder = String(repeating: "a", count: expectedLength)
     }
     
     var body: some View {
@@ -45,9 +24,8 @@ struct ExpectedText: View {
                 Text(text)
                     .transition(.scale)
             } else {
-                MockTextView()
-                    .frame(height: font.leadingHeight)
-                    .transition(.opacity)
+                Text(verbatim: placeholder)
+                    .redacted(reason: .placeholder)
             }
         }
         .animation(.interactiveSpring, value: text.value != nil)
