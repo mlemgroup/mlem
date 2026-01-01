af9018dc05d6f710888cc7cb00d6e1da1e935c0c
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2026-01-01 12:59:44 +0000

    Use Blockable directly

--- Mlem/App/Actions/BlockAction.swift
+++ Mlem/App/Actions/BlockAction.swift
@@ -10,42 +10,15 @@ import MlemMiddleware
 import SwiftUI
 
 struct BlockAction: Actions.Action {
-    enum Content: Hashable {
-        case person(any Person1Providing)
-        case community(any Community1Providing)
-
-        var entity: any Blockable {
-            switch self {
-            case let .person(person): person
-            case let .community(community): community
-            }
-        }
-
-        var blocked: Bool {
-            switch self {
-            case let .person(person): person.blocked
-            case let .community(community): community.blocked
-            }
-        }
-
-        static func == (lhs: Self, rhs: Self) -> Bool {
-            lhs.entity.actorId == rhs.entity.actorId
-        }
-
-        func hash(into hasher: inout Hasher) {
-            hasher.combine(entity.actorId)
-        }
-    }
-
     enum Relationship { case direct, indirect }
 
     enum ContentType { case personOnly, communityOnly, multi }
 
-    let content: Set<Content>
+    let content: [any Blockable]
     let relationship: Relationship
 }
 
-extension Set<BlockAction.Content> {
+private extension [any Blockable] {
     var contentType: BlockAction.ContentType {
         if self.count > 1 {
             return .multi
@@ -56,8 +29,9 @@ extension Set<BlockAction.Content> {
         } 
 
         return switch first {
-        case .person: .personOnly
-        case .community: .communityOnly
+        case is any Person: .personOnly
+        case is any Community: .communityOnly
+        default: .multi
         }
     }
 }
@@ -70,7 +44,7 @@ extension ActionSeed {
         label: BlockAction.createLabel(relationship: .direct, mode: .block, contentType: .multi)
     ) { entity in
         switch entity {
-        case let entity as any Person1Providing: BlockAction(content: [.person(entity)], relationship: .direct)
+        case let entity as any Person1Providing: BlockAction(content: [entity], relationship: .direct)
         default: nil
         }
     }
@@ -81,11 +55,11 @@ extension ActionSeed {
     ) { entity in
         switch entity {
         case let entity as any Comment2Providing: BlockAction(
-            content: [.person(entity.creator)],
+            content: [entity.creator],
             relationship: .indirect
         )
         case let entity as any Post2Providing: BlockAction(
-            content: [.person(entity.creator), .community(entity.community)],
+            content: [entity.creator, entity.community],
             relationship: .indirect
         )
         default: nil
@@ -128,13 +102,13 @@ extension BlockAction {
     func createLabel(environment: EnvironmentValues) -> ActionLabel {
         Self.createLabel(
             relationship: self.relationship,
-            mode: content.first!.entity.blocked ? .unblock : .block,
+            mode: content.first!.blocked ? .unblock : .block,
             contentType: content.contentType
         ).withVisibility(visibility(environment))
     }
 
     private func visibility(_ environment: EnvironmentValues) -> ActionVisiblity {
-        let canInteract = content.allSatisfy { $0.entity.api.canInteract(appState: environment.appState) }
+        let canInteract = content.allSatisfy { $0.api.canInteract(appState: environment.appState) }
         guard canInteract else { return .hidden }
 
         guard let first = content.first else {
@@ -142,8 +116,8 @@ extension BlockAction {
             return .hidden
         }
         
-        if let person = content.compactMap({ $0.entity as? any Person }).first {
-            guard let myPersonId = first.entity.api.myPerson?.id else { return .hidden }
+        if let person = content.compactMap({ $0 as? any Person }).first {
+            guard let myPersonId = first.api.myPerson?.id else { return .hidden }
             guard person.id != myPersonId else { return .hidden }
         }
 
@@ -166,12 +140,12 @@ extension BlockAction {
             return
         }
 
-        execute(entity: first.entity, environment: environment)
+        execute(entity: first, environment: environment)
     }
 
     @MainActor
     func executeMulti(environment: EnvironmentValues) {
-        var actions: [PopupAnchorModel.Action] = content.map(\.entity).map { entity in
+        let actions: [PopupAnchorModel.Action] = content.map { entity in
             .init(title: entity is any Person ? "User" : "Community", isDestructive: true) {
                 Task {
                     let response = await entity.toggleBlocked().value
