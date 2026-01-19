fa4fb18010f9a1b3d2249131506e527925f99f61
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2026-01-18 19:59:50 -0500

    tidy

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Shared/ExpectedValue.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Shared/ExpectedValue.swift
@@ -25,28 +25,8 @@ public struct ExpectedValue<T> {
         }
     }
     
-//    mutating func udpateValue(_ newValue: T) {
-//        self.value_ = newValue
-//    }
-    
     /// When called, updates the value provider (i.e., what getValue() reads from) to include this value.
     let provideValue: () async throws -> Void
-    
-    /// Provides the value currently stored in this ExpectedValue. If the value is not present,
-    /// it is automatically fetched
-//    public var value: T? {
-//        get {
-//            if let ret = getValue() { return ret }
-//            Task {
-//                do {
-//                    try await provideValue()
-//                } catch {
-//                    print(error)
-//                }
-//            }
-//            return nil
-//        }
-//    }
     
     init(value: T?, provideValue: @escaping () async throws -> Void) {
         self.value_ = value
