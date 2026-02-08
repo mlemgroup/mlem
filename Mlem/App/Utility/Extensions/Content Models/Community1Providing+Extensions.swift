393a43f8e0bf080d574961c1670df2a1c71d3e38
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2026-02-08 11:02:54 -0500

    tidy

--- Mlem/App/Utility/Extensions/Content Models/Community1Providing+Extensions.swift
+++ Mlem/App/Utility/Extensions/Content Models/Community1Providing+Extensions.swift
@@ -81,35 +81,6 @@ extension Community1Providing {
         }
     }
     
-//    func toggleBlocked(feedback: Set<FeedbackType>) {
-//        if feedback.contains(.toast) {
-//            if !blocked {
-//                ToastModel.main.add(
-//                    .undoable(
-//                        "Blocked",
-//                        icon: .lemmy.block,
-//                        callback: {
-//                            self.updateBlocked(false)
-//                        },
-//                        color: .themedNegative
-//                    )
-//                )
-//            } else {
-//                ToastModel.main.add(
-//                    .undoable(
-//                        "Unblocked",
-//                        icon: .lemmy.unblock,
-//                        callback: {
-//                            self.updateBlocked(true)
-//                        },
-//                        color: .themedPrimary
-//                    )
-//                )
-//            }
-//        }
-//        toggleBlocked()
-//    }
-    
     // MARK: Action Collections
     
     @ActionBuilder
--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Person/Person+Conformance.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Person/Person+Conformance.swift
@@ -143,35 +143,3 @@ public extension Person {
         )
     }
 }
-
-// TDOO: NOW
-//public extension Person2 {
-//    struct CodedData: Codable {
-//        let apiUrl: URL
-//        let apiMyPersonId: Int?
-//        let apiPersonView: LemmyPersonView
-//    }
-//
-//    internal var apiPersonView: LemmyPersonView {
-//        .init(
-//            person: person1.apiPerson,
-//            counts: .init(
-//                personId: id,
-//                postCount: postCount,
-//                commentCount: commentCount
-//            ),
-//            isAdmin: isAdmin,
-//            personActions: nil,
-//            banned: nil,
-//            banExpiresAt: nil
-//        )
-//    }
-//
-//    func codedData() async throws -> CodedData {
-//        try await .init(
-//            apiUrl: api.baseUrl,
-//            apiMyPersonId: api.myPersonId,
-//            apiPersonView: apiPersonView
-//        )
-//    }
-//}
