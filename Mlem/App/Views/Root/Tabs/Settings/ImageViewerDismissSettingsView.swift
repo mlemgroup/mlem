59dc509da6c9a3fbe4e8d2e4705765546eb7166b
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2026-03-20 12:01:51 +0000

    Refactor `ImageViewerDismissSettingsView` body

--- Mlem/App/Views/Root/Tabs/Settings/ImageViewerDismissSettingsView.swift
+++ Mlem/App/Views/Root/Tabs/Settings/ImageViewerDismissSettingsView.swift
@@ -14,38 +14,47 @@ struct ImageViewerDismissSettingsView: View {
 
     var body: some View {
         Form {
-            SettingsHeaderView(
-                title: "Dismiss Sensitivity",
-                description: "Choose how far you have to drag to dismiss the image viewer.",
-                icon: .settings.imageViewerDismissSensitivity
-            )
-            .gradientTint(.themedColorfulAccent(5))
-
-            Section {
-                VStack {
-                    HStack {
-                        Text("Drag Less")
-                        Spacer()
-                        Text("Drag More")
-                    }
-                    .font(.footnote)
-                    .foregroundStyle(.themedSecondary)
-
-                    // I tried using Binding(get: set:) here, but it caused haptics to be
-                    // spammed if you move the handle to either end of the slider.
-                    Slider(
-                        value: $sliderValue,
-                        in: 2...20,
-                        step: 1
-                    )
-                    .onChange(of: sliderValue) {
-                        self.dismissThreshold = Int(sliderValue.rounded())
-                    }
-                }
-            }
+            headerView
+            sliderView
         }
         .withConditionalLabelStyle()
         .contentMargins(.top, 16)
         .hiddenNavigationTitle("Dismiss Sensitivity")
+    }
+
+    @ViewBuilder
+    var headerView: some View {
+        SettingsHeaderView(
+            title: "Dismiss Sensitivity",
+            description: "Choose how far you have to drag to dismiss the image viewer.",
+            icon: .settings.imageViewerDismissSensitivity
+        )
+        .gradientTint(.themedColorfulAccent(5))
+    }
+
+    @ViewBuilder
+    var sliderView: some View {
+        Section {
+            VStack(spacing: 5) {
+                HStack {
+                    Text("Drag Less")
+                    Spacer()
+                    Text("Drag More")
+                }
+                .font(.footnote)
+                .foregroundStyle(.themedSecondary)
+
+                // I tried using Binding(get: set:) here, but it caused haptics to be
+                // spammed if you move the handle to either end of the slider.
+                Slider(
+                    value: $sliderValue,
+                    in: 2...20,
+                    step: 1
+                )
+                .onChange(of: sliderValue) {
+                    self.dismissThreshold = Int(sliderValue.rounded())
+                }
+            }
+        }
     }
 }
