ae0bae08d02a6294797c3f7a9d17329beb600242
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2026-04-06 21:12:00 -0400

    update RealizedValue spec

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Shared/ValueProviding/RealizedValue.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/Shared/ValueProviding/RealizedValue.swift
@@ -5,12 +5,16 @@
 //  Created by Eric Andrews on 2026-02-25.
 //
 
-public struct RealizedValue<T>: RealizedValueProviding {
-    private let value_: T
+public class RealizedValue<T>: RealizedValueProviding {
+    private var value_: T
     public var value: T? { value_ }
     public var realizedValue: T { value_ }
     
     public init(_ value: T) {
         self.value_ = value
+    }
+    
+    public func set(_ newValue: T) {
+        value_ = newValue
     }
 }
