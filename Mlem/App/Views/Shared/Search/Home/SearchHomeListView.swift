eafeec4dad1fd9aad8ebfe27467c1f2accc3f1a7
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2026-04-23 19:15:37 +0100

    Add `SearchHomeListView`

---
+++ Mlem/App/Views/Shared/Search/Home/SearchHomeListView.swift
@@ -0,0 +1,34 @@
+//
+//  SearchHomeListView.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2026-04-23.
+//
+
+import SwiftUI
+
+struct SearchHomeListView<Content: View>: View {
+    var content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View {
+        VStack {
+            Group(subviews: content) { subviews in
+                ForEach(Array(subviews.enumerated()), id: \.element.id) { index, subview in
+                    subview
+                    if index != subviews.count - 1 {
+                        Divider()
+                            .padding(.leading, 50)
+                    }
+                }
+            }
+        }
+        .padding(10)
+        .padding(.trailing, 5)
+        .background(.themedSecondaryGroupedBackground, in: .rect(cornerRadius: 25))
+        .paletteBorder(cornerRadius: 25)
+    }
+}
