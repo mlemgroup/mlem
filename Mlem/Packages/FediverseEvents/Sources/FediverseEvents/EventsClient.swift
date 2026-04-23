1acca080a8812bb87456d9ae35fc8df8426fc155
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2026-04-23 15:28:13 +0100

    Add `RestClient` instance to `EventsClient`

--- Mlem/Packages/FediverseEvents/Sources/FediverseEvents/EventsClient.swift
+++ Mlem/Packages/FediverseEvents/Sources/FediverseEvents/EventsClient.swift
@@ -7,8 +7,30 @@
 
 import Foundation
 import Observation
+import Rest
+
+public enum EventsEnvironment {
+    case qualityControl, production
+    
+    var address: URL {
+        switch self {
+        case .production: .init(string: "https://api.fediverse.events/v1")!
+        case .qualityControl: .init(string: "https://test-api.fediverse.events/v1")!
+        }
+    }
+}
 
 @Observable
 public final class EventsClient {
+    internal let restClient = RestClient(convertParamsToSnakeCase: false, decoder: .defaultDecoder)
+
+    public internal(set) var environment: EventsEnvironment = .production
+
+    internal var baseUrl: URL { environment.address }
+
     public init() {}
+
+    public func changeEnvironment(to environment: EventsEnvironment) {
+        self.environment = environment
+    }
 }
