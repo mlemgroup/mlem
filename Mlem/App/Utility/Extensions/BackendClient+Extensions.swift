a681e64db3ece5ff685c6018435628dfaef3403b
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2026-04-23 15:34:58 +0100

    Move `BackendClient.main` to Mlem

---
+++ Mlem/App/Utility/Extensions/BackendClient+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  MlemBackend+Extensions.swift
+//  Mlem
+//
+//  Created by Sjmarf on 2026-04-23.
+//
+
+import MlemBackend
+
+extension BackendClient {
+    static var main: BackendClient = .init()
+}
