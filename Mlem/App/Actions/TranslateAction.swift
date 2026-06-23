ac090d66192ea435d3332d95040e52ae7273c576
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2026-06-23 10:35:48 +0100

    Fall back to language detection if language ID matches system language

--- Mlem/App/Actions/TranslateAction.swift
+++ Mlem/App/Actions/TranslateAction.swift
@@ -133,7 +133,9 @@ extension TranslateAction {
     }
 
     private func determineLanguage(of text: String) async throws -> Locale.Language {
-        if let myInstance = entity.api.myInstance, let language = myInstance.language(withId: entity.languageId) {
+        if let myInstance = entity.api.myInstance,
+            let language = myInstance.language(withId: entity.languageId),
+            language != Locale.current.language {
             return language
         }
 
