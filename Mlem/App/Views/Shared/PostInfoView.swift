009034049f3e5d34f9ea1d685e3e1299273a2683
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2026-06-24 20:45:08 -0400

    add poll

--- Mlem/App/Views/Shared/PostInfoView.swift
+++ Mlem/App/Views/Shared/PostInfoView.swift
@@ -20,6 +20,37 @@ private struct InfoEntry {
     }
 }
 
+private struct InfoSubEntry {
+    let title: LocalizedStringResource?
+    let value: CustomStringConvertible?
+    
+    init(_ title: LocalizedStringResource?, value: CustomStringConvertible?) {
+        self.title = title
+        self.value = value
+    }
+    
+    var text: Text {
+        titleText + valueText
+    }
+    
+    private var titleText: Text {
+        if let title {
+            Text(title) +
+            Text(verbatim: ": ")
+        } else {
+            Text(verbatim: "")
+        }
+    }
+    
+    private var valueText: Text {
+        if let value {
+            return Text(verbatim: "\(value)")
+        }
+        return Text("None")
+            .foregroundStyle(.themedSecondary)
+    }
+}
+
 struct PostInfoView: View {
     @Environment(\.locale) var locale
     
@@ -37,6 +68,11 @@ struct PostInfoView: View {
         ScrollView {
             VStack(spacing: 16) {
                 section {
+                    entry(.init("Local ID", value: post.id))
+                    entry(.init("Actor ID", value: post.actorId))
+                }
+                
+                section {
                     entry(.init("Created", value: post.created))
                     entry(.init("Last Updated", value: post.updated))
                     entry(.init("Language", value: language))
@@ -53,12 +89,14 @@ struct PostInfoView: View {
                     if let creator = post.creator.value,
                        let creatorIsModerator = post.creatorIsModerator.value,
                        let creatorIsAdmin = post.creatorIsAdmin.value,
-                       let creatorBannedFromCommunity = post.creatorBannedFromCommunity.value {
+                       let creatorBannedFromCommunity = post.creatorBannedFromCommunity.value,
+                       let creatorBlocked = post.creatorBlocked.value {
                         entry(.init("Creator", value: creator.actorId), subEntries: [
                             .init("ID", value: post.creatorId),
                             .init("Moderator", value: creatorIsModerator),
                             .init("Admin", value: creatorIsAdmin),
-                            .init("Banned from community", value: creatorBannedFromCommunity)
+                            .init("Banned from community", value: creatorBannedFromCommunity),
+                            .init("Blocked", value: creatorBlocked)
                         ])
                     }
                     ExpectedView(post.community) { community in
@@ -75,8 +113,41 @@ struct PostInfoView: View {
                 }
                 
                 section {
-                    entry(.init("Local ID", value: post.id))
-                    entry(.init("Actor ID", value: post.actorId))
+                    ExpectedView(post.commentCount) { commentCount in
+                        entry(.init("Comments", value: commentCount, copyable: false))
+                    }
+                    ExpectedView(post.unreadCommentCount) { unreadCommentCount in
+                        entry(.init("Unread Comments", value: unreadCommentCount, copyable: false))
+                    }
+                    ExpectedView(post.votes) { votes in
+                        entry(.init("Score", value: votes.total, copyable: false), subEntries: [
+                            .init("Upvotes", value: votes.upvotes),
+                            .init("Downvotes", value: votes.downvotes),
+                            .init("My vote", value: votes.myVote)
+                        ])
+                    }
+                    ExpectedView(post.crossPosts) { crossPosts in
+                        entry(
+                            .init("Crossposts", value: crossPosts.count, copyable: false),
+                            subEntries: crossPosts.compactMap { crossPost in
+                                if let community = crossPost.community.value {
+                                    return InfoSubEntry(nil, value: community.actorId)
+                                }
+                                return nil
+                            })
+                    }
+                    ExpectedView(post.saved) { saved in
+                        entry(.init("Saved", value: saved, copyable: false))
+                    }
+                    ExpectedView(post.notificationsEnabled) { notificationsEnabled in
+                        entry(.init("Notifications Enabled", value: notificationsEnabled, copyable: false))
+                    }
+                    ExpectedView(post.read) { read in
+                        entry(.init("Read", value: read, copyable: false))
+                    }
+                    ExpectedView(post.hidden) { hidden in
+                        entry(.init("Hidden", value: hidden, copyable: false))
+                    }
                 }
             }
             .padding(.top, 16)
@@ -96,7 +167,7 @@ struct PostInfoView: View {
         .padding(.horizontal, 16)
     }
     
-    private func entry(_ entry: InfoEntry, subEntries: [InfoEntry] = []) -> some View {
+    private func entry(_ entry: InfoEntry, subEntries: [InfoSubEntry] = []) -> some View {
         VStack(alignment: .leading, spacing: 4) {
             Text(entry.title).fontWeight(.semibold)
             if let value = entry.value {
@@ -119,22 +190,14 @@ struct PostInfoView: View {
                     .foregroundStyle(.themedSecondary)
             }
             
-            ForEach(subEntries, id: \.title.key) { subEntry in
-                Text("\(Image(systemName: "arrow.turn.down.right")) ").foregroundStyle(.themedSecondary) +
-                Text(subEntry.title) +
-                Text(": ") +
-                subEntryValue(subEntry)
+            ForEach(Array(zip(subEntries.indices, subEntries)), id: \.0) { _, subEntry in
+                HStack(alignment: .top, spacing: 2) {
+                    Text("\(Image(systemName: "arrow.turn.down.right")) ").foregroundStyle(.themedSecondary)
+                    subEntry.text
+                }
             }
         }
         .font(.subheadline)
-    }
-    
-    private func subEntryValue(_ subEntry: InfoEntry) -> Text {
-        if let value = subEntry.value {
-            return Text(verbatim: "\(value)")
-        }
-        return Text("None")
-            .foregroundStyle(.themedSecondary)
     }
     
     @ViewBuilder
