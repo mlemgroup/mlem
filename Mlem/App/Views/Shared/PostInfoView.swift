9b029c3dda06971aa1358fda26c72fc95fe58549
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2026-06-27 11:12:31 -0400

    tidy up

--- Mlem/App/Views/Shared/PostInfoView.swift
+++ Mlem/App/Views/Shared/PostInfoView.swift
@@ -9,45 +9,30 @@ import SwiftUI
 import MlemMiddleware
 
 private struct InfoEntry {
+    /// Name of the entry
     let title: LocalizedStringResource
-    let value: CustomStringConvertible?
+    
+    /// Values, if present, associated with the entry
+    let values: [CustomStringConvertible]?
+    
+    /// Whether entries should copy to clipboard on tap
     let copyable: Bool
     
-    init(_ title: LocalizedStringResource, value: CustomStringConvertible?, copyable: Bool = true) {
+    /// Whether `nil` indicates a contentful absence of value
+    let valueExpected: Bool
+    
+    init(_ title: LocalizedStringResource, value: CustomStringConvertible?, copyable: Bool = false, valueExpected: Bool = true) {
         self.title = title
-        self.value = value
+        self.values = value.map { [$0] }
         self.copyable = copyable
+        self.valueExpected = valueExpected
     }
-}
-
-private struct InfoSubEntry {
-    let title: LocalizedStringResource?
-    let value: CustomStringConvertible?
     
-    init(_ title: LocalizedStringResource?, value: CustomStringConvertible?) {
+    init(_ title: LocalizedStringResource, values: [CustomStringConvertible]?, copyable: Bool = false, valueExpected: Bool = true) {
         self.title = title
-        self.value = value
-    }
-    
-    var text: Text {
-        titleText + valueText
-    }
-    
-    private var titleText: Text {
-        if let title {
-            Text(title) +
-            Text(verbatim: ": ")
-        } else {
-            Text(verbatim: "")
-        }
-    }
-    
-    private var valueText: Text {
-        if let value {
-            return Text(verbatim: "\(value)")
-        }
-        return Text("None")
-            .foregroundStyle(.themedSecondary)
+        self.values = values
+        self.copyable = copyable
+        self.valueExpected = valueExpected
     }
 }
 
@@ -68,31 +53,31 @@ struct PostInfoView: View {
         ScrollView {
             VStack(spacing: 16) {
                 section {
-                    entry(.init("Local ID", value: post.id))
-                    entry(.init("Actor ID", value: post.actorId))
+                    entry(.init("Local ID", value: post.id, copyable: true))
+                    entry(.init("Actor ID", value: post.actorId, copyable: true))
                 }
                 
                 section {
-                    entry(.init("Created", value: post.created))
-                    entry(.init("Last Updated", value: post.updated))
-                    entry(.init("Language", value: language))
-                    entry(.init("NSFW", value: post.nsfw, copyable: false))
-                    entry(.init("Deleted", value: post.deleted, copyable: false))
-                    entry(.init("Removed", value: post.removed, copyable: false))
-                    entry(.init("Locked", value: post.locked, copyable: false))
-                    entry(.init("Pinned to Community", value: post.pinnedCommunity, copyable: false))
-                    entry(.init("Pinned to Instance", value: post.pinnedInstance, copyable: false))
+                    entry(.init("Created", value: post.created, copyable: true))
+                    entry(.init("Last Updated", value: post.updated, copyable: true))
+                    entry(.init("Language", value: language, copyable: true))
+                    entry(.init("NSFW", value: post.nsfw))
+                    entry(.init("Deleted", value: post.deleted))
+                    entry(.init("Removed", value: post.removed))
+                    entry(.init("Locked", value: post.locked))
+                    entry(.init("Pinned to Community", value: post.pinnedCommunity))
+                    entry(.init("Pinned to Instance", value: post.pinnedInstance))
                 }
                 
                 section {
-                    entry(.init("Title", value: post.title))
+                    entry(.init("Title", value: post.title, copyable: true))
                     if let creator = post.creator.value,
                        let creatorIsModerator = post.creatorIsModerator.value,
                        let creatorIsAdmin = post.creatorIsAdmin.value,
                        let creatorBannedFromCommunity = post.creatorBannedFromCommunity.value,
                        let creatorBlocked = post.creatorBlocked.value {
-                        entry(.init("Creator", value: creator.actorId), subEntries: [
-                            .init("ID", value: post.creatorId),
+                        entry(.init("Creator", value: creator.actorId, copyable: true), subEntries: [
+                            .init("Local ID", value: post.creatorId, copyable: true),
                             .init("Moderator", value: creatorIsModerator),
                             .init("Admin", value: creatorIsAdmin),
                             .init("Banned from community", value: creatorBannedFromCommunity),
@@ -101,52 +86,49 @@ struct PostInfoView: View {
                     }
                     ExpectedView(post.community) { community in
                         entry(.init("Community", value: community.actorId), subEntries: [
-                            .init("ID", value: post.communityId)
+                            .init("Local ID", value: post.communityId)
                         ])
                     }
-                    entry(.init("Content", value: post.content))
-                    entry(.init("Link URL", value: post.linkUrl))
-                    entry(.init("Thumbnail URL", value: post.thumbnailUrl))
+                    entry(.init("Content", value: post.content, copyable: true))
+                    entry(.init("Link URL", value: post.linkUrl, copyable: true))
+                    entry(.init("Thumbnail URL", value: post.thumbnailUrl, copyable: true))
                     embed(post.embed)
                     poll(post.poll)
-                    entry(.init("Alt Text", value: post.altText))
+                    entry(.init("Alt Text", value: post.altText, copyable: true))
                 }
                 
                 section {
                     ExpectedView(post.commentCount) { commentCount in
-                        entry(.init("Comments", value: commentCount, copyable: false))
+                        entry(.init("Comments", value: commentCount))
                     }
                     ExpectedView(post.unreadCommentCount) { unreadCommentCount in
-                        entry(.init("Unread Comments", value: unreadCommentCount, copyable: false))
+                        entry(.init("Unread Comments", value: unreadCommentCount))
                     }
                     ExpectedView(post.votes) { votes in
-                        entry(.init("Score", value: votes.total, copyable: false), subEntries: [
-                            .init("Upvotes", value: votes.upvotes),
-                            .init("Downvotes", value: votes.downvotes),
-                            .init("My vote", value: votes.myVote)
-                        ])
+                        VStack(alignment: .leading, spacing: 2) {
+                            entry(.init("Score", value: nil, valueExpected: false))
+                            entryGrid([
+                                .init("Total", value: votes.total),
+                                .init("Upvotes", value: votes.upvotes),
+                                .init("Downvotes", value: votes.downvotes),
+                                .init("My vote", value: votes.myVote)
+                            ], isSubEntry: false)
+                        }
                     }
                     ExpectedView(post.crossPosts) { crossPosts in
-                        entry(
-                            .init("Crossposts", value: crossPosts.count, copyable: false),
-                            subEntries: crossPosts.compactMap { crossPost in
-                                if let community = crossPost.community.value {
-                                    return InfoSubEntry(nil, value: community.actorId)
-                                }
-                                return nil
-                            })
+                        entry(.init("Crossposts", values: crossPosts.map { $0.actorId.description }, copyable: true))
                     }
                     ExpectedView(post.saved) { saved in
-                        entry(.init("Saved", value: saved, copyable: false))
+                        entry(.init("Saved", value: saved))
                     }
                     ExpectedView(post.notificationsEnabled) { notificationsEnabled in
-                        entry(.init("Notifications Enabled", value: notificationsEnabled, copyable: false))
+                        entry(.init("Notifications Enabled", value: notificationsEnabled))
                     }
                     ExpectedView(post.read) { read in
-                        entry(.init("Read", value: read, copyable: false))
+                        entry(.init("Read", value: read))
                     }
                     ExpectedView(post.hidden) { hidden in
-                        entry(.init("Hidden", value: hidden, copyable: false))
+                        entry(.init("Hidden", value: hidden))
                     }
                 }
             }
@@ -167,45 +149,73 @@ struct PostInfoView: View {
         .padding(.horizontal, 16)
     }
     
-    private func entry(_ entry: InfoEntry, subEntries: [InfoSubEntry] = []) -> some View {
+    private func entry(_ entry: InfoEntry, subEntries: [InfoEntry]? = nil) -> some View {
         VStack(alignment: .leading, spacing: 4) {
             Text(entry.title).fontWeight(.semibold)
-            if let value = entry.value {
-                if value.description.isEmpty {
-                    Text("Empty").foregroundStyle(.themedSecondary)
-                } else {
-                    Button {
-                        if entry.copyable {
-                            UIPasteboard.general.string = "\(value)"
-                            ToastModel.main.add(.success("Copied"))
-                        }
-                    } label: {
-                        Text(verbatim: "\(value)")
-                    }
-                    .contentShape(.rect)
-                    .buttonStyle(.plain)
-                }
-            } else {
-                Text("None")
-                    .foregroundStyle(.themedSecondary)
-            }
-            
-            ForEach(Array(zip(subEntries.indices, subEntries)), id: \.0) { _, subEntry in
-                HStack(alignment: .top, spacing: 2) {
-                    Text("\(Image(systemName: "arrow.turn.down.right")) ").foregroundStyle(.themedSecondary)
-                    subEntry.text
-                }
+            values(for: entry)
+            if let subEntries {
+                entryGrid(subEntries)
             }
         }
         .font(.subheadline)
     }
     
     @ViewBuilder
+    private func values(for entry: InfoEntry) -> some View {
+        if entry.valueExpected {
+            if let values = entry.values {
+                ForEach(Array(zip(values.indices, values)), id: \.0) { _, value in
+                    if value.description.isEmpty {
+                        Text("Empty").foregroundStyle(.themedSecondary)
+                    } else {
+                        Button {
+                            if entry.copyable {
+                                UIPasteboard.general.string = "\(value)"
+                                ToastModel.main.add(.success("Copied"))
+                            }
+                        } label: {
+                            Text(verbatim: "\(value)")
+                        }
+                        .contentShape(.rect)
+                        .buttonStyle(.plain)
+                    }
+                }
+            } else {
+                Text("None")
+                    .foregroundStyle(.themedSecondary)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func entryGrid(_ entries: [InfoEntry], isSubEntry: Bool = true) -> some View {
+        Grid(alignment: .top, verticalSpacing: 2) {
+            ForEach(Array(zip(entries.indices, entries)), id: \.0) { _, entry in
+                GridRow {
+                    Text(entry.title)
+                    values(for: entry)
+                }
+                .gridColumnAlignment(.leading)
+            }
+        }
+        .font(isSubEntry ? .footnote : .subheadline)
+        .padding(.leading, isSubEntry ? 13 : 0)
+    }
+    
+    @ViewBuilder
+    private func subEntry(_ text: Text) -> some View {
+        HStack(alignment: .top, spacing: 2) {
+            Text("\(Image(systemName: "arrow.turn.down.right")) ").foregroundStyle(.themedSecondary)
+            text
+        }
+    }
+    
+    @ViewBuilder
     private func embed(_ embed: PostEmbed?) -> some View {
         if let embed {
-            entry(.init("Embedded Content", value: embed.title ?? "Untitled"), subEntries: [
-                .init("Description", value: embed.description),
-                .init("Video URL", value: embed.videoUrl)
+            entry(.init("Embedded Content", value: embed.title ?? "Untitled", copyable: embed.title != nil), subEntries: [
+                .init("Description", value: embed.description, copyable: true),
+                .init("Video URL", value: embed.videoUrl, copyable: true)
             ])
         } else {
             entry(.init("Embedded Content", value: nil))
@@ -215,11 +225,18 @@ struct PostInfoView: View {
     @ViewBuilder
     private func poll(_ poll: PostPoll?) -> some View {
         if let poll {
-            entry(.init("Poll", value: poll.type.description), subEntries: [
-                .init("End date", value: poll.endDate),
-                .init("Local only", value: poll.localOnly),
-                .init("Latest vote", value: poll.latestVote)
-            ])
+            VStack(alignment: .leading, spacing: 2) {
+                entry(.init("Poll", value: nil, valueExpected: false))
+                entryGrid([
+                    .init("Type", value: poll.type.description, copyable: true),
+                    .init("Latest vote", value: poll.latestVote, copyable: true),
+                    .init("End date", value: poll.endDate, copyable: true),
+                    .init("Local only", value: poll.localOnly),
+                    .init("Choices", value: nil, valueExpected: false)
+                ], isSubEntry: false)
+                entryGrid(poll.choices.map { .init("\($0.voteCount ?? 0)", value: $0.label) })
+            }
+            .font(.subheadline)
         } else {
             entry(.init("Poll", value: nil))
         }
