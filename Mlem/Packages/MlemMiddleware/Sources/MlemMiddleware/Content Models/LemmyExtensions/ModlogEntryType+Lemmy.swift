df5d7e1c5a090ce4736f19ed197019bd58e06bc4
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2026-06-28 07:43:44 +0100

    Move `ModlogEntryType` initialiser to `+Lemmy`

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/LemmyExtensions/ModlogEntryType+Lemmy.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/Content Models/LemmyExtensions/ModlogEntryType+Lemmy.swift
@@ -8,6 +8,40 @@
 import Foundation
 
 extension ModlogEntryType {
+    init?(from type: LemmyModlogKind) throws(ApiClientError) {
+        let result: Self? = switch type {
+        case .all: nil
+        case .modRemovePost: .removePost
+        case .modLockPost: .lockPost
+        case .modFeaturePost: .pinPost
+        case .modRemoveComment: .removeComment
+        case .modBanFromCommunity: .banPersonFromCommunity
+        case .modAddToCommunity, .modAddCommunity: .updatePersonModeratorStatus
+        case .modTransferCommunity: .transferCommunityOwnership
+        case .modHideCommunity: .hideCommunity
+        case .adminAdd, .modAdd: .updatePersonAdminStatus
+        case .adminBan, .modBan: .banPersonFromInstance
+        case .adminRemoveCommunity, .modRemoveCommunity: .removeCommunity
+        case .adminPurgePerson: .purgePerson
+        case .adminPurgeCommunity: .purgeCommunity
+        case .adminPurgePost: .purgePost
+        case .adminPurgeComment: .purgeComment
+        case .modChangeCommunityVisibility: throw .featureUnsupported
+        case .adminBlockInstance: throw .featureUnsupported
+        case .adminAllowInstance: throw .featureUnsupported
+        case .modLockComment: throw .featureUnsupported
+        case .adminFeaturePostSite: throw .featureUnsupported
+        case .modFeaturePostCommunity: throw .featureUnsupported
+        case .modWarnPost: throw .featureUnsupported
+        case .modWarnComment: throw .featureUnsupported
+        }
+        if let result {
+            self = result
+        } else {
+            return nil
+        }
+    }
+
     var lemmyApiType: LemmyModlogKind {
         switch self {
         case .removePost: .modRemovePost
