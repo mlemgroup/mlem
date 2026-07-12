b0434a1bbfbe1a57ef436464f2e075b5a2a1178f
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2026-07-12 15:58:41 +0100

    Fix sidebar navigation on iPad (#2861)

--- Mlem/App/Views/Shared/Navigation/NavigationFrame.swift
+++ Mlem/App/Views/Shared/Navigation/NavigationFrame.swift
@@ -12,7 +12,8 @@ import Foundation
 
 @Observable
 class NavigationFrame: Hashable {
-    var page: NavigationPage
+    private(set) var page: NavigationPage
+    private var updateCount: Int = 0
 
     init(page: NavigationPage) {
         self.page = page
@@ -20,6 +21,18 @@ class NavigationFrame: Hashable {
 
     func hash(into hasher: inout Hasher) {
         hasher.combine(ObjectIdentifier(self))
+    }
+
+    func replacePage(with page: NavigationPage) {
+        self.page = page
+        self.updateCount += 1
+    }
+
+    var updateCountHash: Int {
+        var hasher = Hasher()
+        hasher.combine(ObjectIdentifier(self))
+        hasher.combine(updateCount)
+        return hasher.finalize()
     }
 
     public static func == (lhs: NavigationFrame, rhs: NavigationFrame) -> Bool {
