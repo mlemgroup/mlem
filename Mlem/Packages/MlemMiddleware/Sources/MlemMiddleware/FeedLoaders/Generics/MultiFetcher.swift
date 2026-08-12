cf54e660967004658db1f2231c0c8fa4f7ad7286
Eric Andrews <eric.b.andrews.auto@protonmail.com>
2026-08-12 11:25:18 -0400

    Parallelize initial MultiFetcher fetch (#2910)

--- Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/FeedLoaders/Generics/MultiFetcher.swift
+++ Mlem/Packages/MlemMiddleware/Sources/MlemMiddleware/FeedLoaders/Generics/MultiFetcher.swift
@@ -20,6 +20,21 @@ class MultiFetcher<Item: FeedLoadable>: Fetcher<Item> {
     }
     
     override func fetch() async throws -> LoadingResponse<Item> {
+        // preemptively trigger loading on all child feed loaders if they have not loaded anything yet
+        await withTaskGroup(of: Void.self) { taskGroup in
+            for source in sources where source.fetcher.location == .start {
+                taskGroup.addTask {
+                    do {
+                        try await source.loadMoreItems()
+                    } catch {
+                        self.log.error("\(error.localizedDescription)")
+                        // noop - if the error recurs it will be caught by normal error handling below.
+                        // prefetch is not required to succeed for loading to work
+                    }
+                }
+            }
+        }
+        
         var newItems: [Item] = .init()
         
         while newItems.count < pageSize {
